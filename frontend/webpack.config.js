08cbfd80198ca03d8aff9219347977f6f4882eca
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-07-11 20:22:06 +0900

    chore: webpack 설정
    Co-authored-by: Youngjin Park <youngjin988@gmail.com>
    Co-authored-by: Nangkyeong Lim <nangkyeonglim@users.noreply.github.com>

---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    hot: true,
+    open: true,
+  },
+};

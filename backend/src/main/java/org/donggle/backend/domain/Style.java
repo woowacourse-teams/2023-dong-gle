c1294f5260ace4e554b3ddd63a9905e09c0dc0ed
echo <echo.backend@gmail.com>
2023-07-12 22:18:21 +0900

    feat: 뼈대 코드 구현
    Writing, Block, Style 엔티티 설계
    
    Co-authored-by: HubCreator <chocomilk8604@gmail.com>
    Co-authored-by: ingpyo <juninpyo726@naver.com>
    Co-authored-by: ezzanzzan <eunzzann@gmail.com>

---
+++ backend/src/main/java/org/donggle/backend/domain/Style.java
@@ -0,0 +1,36 @@
+package org.donggle.backend.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Style {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "block_id")
+    private Block block;
+    @Min(0)
+    private int startIndex;
+    @Min(0)
+    private int endIndex;
+    @NotNull
+    @Enumerated(value = EnumType.STRING)
+    private StyleType styleType;
+}
+
---
+++ backend/src/main/java/org/donggle/backend/domain/StyleType.java
@@ -0,0 +1,7 @@
+package org.donggle.backend.domain;
+
+public enum StyleType {
+    BOLD,
+    ITALIC,
+    CODE,
+}

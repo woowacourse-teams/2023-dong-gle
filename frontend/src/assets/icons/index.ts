69073c3e2c976ea09c071d7f56448cc4bffbeb97
yogjin <youngjin988@gmail.com>
2023-07-14 11:53:13 +0900

    chore: svg icon 리소스 추가

---
+++ frontend/src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { ReactComponent as HomeIcon } from './home.svg';
+export { ReactComponent as CalendarIcon } from './calendar.svg';
+export { ReactComponent as PlusCircleIcon } from './plus-circle.svg';
+export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as StatsIcon } from './stats.svg';

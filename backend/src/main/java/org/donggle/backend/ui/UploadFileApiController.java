e604b3bc1487429a5d7a04442f03a62a9f32ca43
HubCreator <chocomilk8604@gmail.com>
2023-07-15 16:59:54 +0900

    feat: CORS 설정 추가

---
+++ backend/src/main/java/org/donggle/backend/ui/UploadFileApiController.java
@@ -0,0 +1,11 @@
+package org.donggle.backend.ui;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/writings/file")
+public class UploadFileApiController {
+
+    
+}

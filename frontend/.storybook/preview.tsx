c04156d13b86753ecddd885d66f3213525e1b71e
yogjin <youngjin988@gmail.com>
2023-07-16 23:28:56 +0900

    chore: storybook msw 설정

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import type { Preview } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
+import { initialize, mswLoader } from 'msw-storybook-addon';
+import { handlers } from '../src/mocks/handlers';
 
 import GlobalStyle from '../src/styles/GlobalStyle';
 import { theme } from '../src/styles/theme';
+
+// msw init
+initialize();
 
 const preview: Preview = {
   parameters: {
@@ -15,6 +20,7 @@ const preview: Preview = {
       },
     },
   },
+  loaders: [mswLoader],
 };
 
 export default preview;

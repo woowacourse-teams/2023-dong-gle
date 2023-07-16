61d61cbdb094456fc05936781e5f5cb6da0d136d
yogjin <youngjin988@gmail.com>
2023-07-16 17:07:46 +0900

    feat: 메인 페이지 중간에서 글을 보여주는 `WritingViewer` 컴포넌트 뼈대 추가

---
+++ frontend/src/components/WritingViewer/WritingViewer.tsx
@@ -0,0 +1,32 @@
+import { getWriting } from 'apis/writings';
+import DOMPurify from 'dompurify';
+import { useGetQuery } from 'hooks/@common/useGetQuery';
+import { styled } from 'styled-components';
+import { GetWritingResponse } from 'types/apis/writings';
+
+type Props = { writingId: number };
+
+const WritingViewer = ({ writingId }: Props) => {
+  const { data, loading } = useGetQuery<GetWritingResponse>({
+    fetcher: () => getWriting(writingId),
+  });
+
+  if (loading) return <div>로딩 중...</div>;
+
+  return (
+    <S.WritingViewerWrapper
+      dangerouslySetInnerHTML={{
+        __html: DOMPurify.sanitize(data?.content ?? '글 내용이 없습니다'),
+      }}
+    ></S.WritingViewerWrapper>
+  );
+};
+
+export default WritingViewer;
+
+const S = {
+  WritingViewerWrapper: styled.section`
+    // TODO: 태그 별 스타일 지정
+    // TODO: <code> 스타일은 highlight.js 고려
+  `,
+};

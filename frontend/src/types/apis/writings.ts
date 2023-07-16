42f8c2f092b5899d45b96622b1e223bb2ba506f2
yogjin <youngjin988@gmail.com>
2023-07-16 16:01:38 +0900

    feat: writings api 타입 추가

---
+++ frontend/src/types/apis/writings.ts
@@ -0,0 +1,20 @@
+export type AddWritingRequest = {
+  file: FormData;
+};
+
+export type GetWritingResponse = {
+  id: number;
+  title: string;
+  content: string;
+};
+
+export type GetWritingPropertiesResponse = {
+  createdAt: Date;
+  isPublished: boolean;
+  publishedAt: Date;
+  publishedTo: string[]; // TODO: Blog string literal 타입으로 변경
+};
+
+export type PublishWritingRequest = {
+  publishTo: string; // TODO: Blog string literal 타입으로 변경
+};

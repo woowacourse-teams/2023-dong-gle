206694315312492fdbfc60117d8cf861d09c9c52
ingpyo <juninpyo726@naver.com>
2023-07-17 17:22:38 +0900

    feat: 주어진 styleType를 제외한 모든 style제거 기능구현

--- backend/src/main/java/org/donggle/backend/domain/parser/MarkDownStyleParser.java
+++ backend/src/main/java/org/donggle/backend/domain/parser/MarkDownStyleParser.java
@@ -7,6 +7,36 @@ import java.util.regex.Pattern;
 
 public class MarkDownStyleParser {
 
+
+    private String removeUnmatchedStyle(String textBlock, StyleType styleType) {
+        StringBuilder buffer = new StringBuilder(textBlock);
+
+        for (StyleType type : StyleType.values()) {
+            if (type != styleType) {
+                Pattern pattern = type.getPattern();
+                Matcher matcher = pattern.matcher(buffer.toString());
+
+                StringBuffer replacementBuffer = new StringBuffer();
+
+                while (matcher.find()) {
+                    if (matcher.groupCount() >= 2) {
+                        String extractedText = matcher.group(2);
+                        if (!extractedText.isEmpty()) { // 그룹이 비어있지 않은 경우에만 대체
+                            matcher.appendReplacement(replacementBuffer, Matcher.quoteReplacement(extractedText));
+                        }
+                    }
+                }
+
+                matcher.appendTail(replacementBuffer);
+
+                buffer = new StringBuilder(replacementBuffer);
+                replacementBuffer.setLength(0); //버퍼 비우기
+            }
+        }
+
+        return buffer.toString();
+    }
+
     private String removePartStyles(String textBlock, StyleType styleType) {
         StringBuilder buffer = new StringBuilder(textBlock);
 

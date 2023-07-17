830cf291f43bacb3715e1a2cc1b998be4d0bbd04
Nangkyeong Lim <57815133+nangkyeonglim@users.noreply.github.com>
2023-07-17 16:06:05 +0900

    [FE] feat: 글 생성 API mocking (#58)
    * feat: 글 생성 POST API mocking 구현
    
    * fix: body 데이터에 body가 한번 더 감싸서 오는 문제 수정

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -2,7 +2,8 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('/test', (req, res, ctx) => {
-    return res(ctx.status(200));
+  // 글 생성(글 업로드): POST
+  rest.post('/writings/file', async (_, res, ctx) => {
+    return res(ctx.status(201), ctx.set('Location', `/writings/1`));
   }),
 ];

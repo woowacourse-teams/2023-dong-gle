b359cccccb9ce6c90cfc13df226944fc8a3eba7a
ezzanzzan <cjsdmswjd821@naver.com>
2023-07-18 13:17:49 +0900

    feat: 객체를 Html 형식으로 변환하는 기능, 테스트 구현
    - HtmlRenderer class 추가
    - HtmlRendererTest class 추가

---
+++ backend/src/main/java/org/donggle/backend/domain/Renderer/Html/HtmlRenderer.java
@@ -0,0 +1,135 @@
+package org.donggle.backend.domain.Renderer.Html;
+
+import org.donggle.backend.domain.Block;
+import org.donggle.backend.domain.BlockType;
+import org.donggle.backend.domain.content.CodeBlockContent;
+import org.donggle.backend.domain.content.Content;
+import org.donggle.backend.domain.content.ImageContent;
+import org.donggle.backend.domain.content.NormalContent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HtmlRenderer {
+    public static final String HTML_TAB = "&emsp;";
+
+    public String render(final List<Block> blocks) {
+        StringBuilder result = new StringBuilder();
+        List<NormalContent> subContent = new ArrayList<>();
+        String temp;
+
+        for (Block block : blocks) {
+            Content content = block.getContent();
+            BlockType blockType = content.getBlockType();
+            temp = "";
+
+            switch (blockType) {
+                case HEADING1, HEADING2, HEADING3, HEADING4, HEADING5, HEADING6, PARAGRAPH, BLOCKQUOTE ->
+                        temp = renderNormalContent((NormalContent) content);
+                case ORDERED_LIST, UNORDERED_LIST -> subContent.add((NormalContent) content);
+                case CODE_BLOCK -> temp = renderCodeBlock((CodeBlockContent) content);
+                //TODO: IMAGE
+            }
+
+            if (!temp.isBlank() && !subContent.isEmpty()) {
+                result.append(renderList(subContent));
+                subContent.clear();
+            }
+            result.append(temp);
+        }
+
+        return String.valueOf(result);
+    }
+
+    public String renderNormalContent(final NormalContent content) {
+        HtmlType htmlType = HtmlType.findByBlockType(content.getBlockType());
+        String depth = renderDepth(content.getDepth());
+        // TODO: style 추가
+        String rawText = content.getRawText();
+
+        return htmlType.getStartTag() + depth + rawText + htmlType.getEndTag();
+    }
+
+    public String renderDepth(final int depth) {
+        return HTML_TAB.repeat(Math.max(0, depth));
+    }
+
+    public String renderCodeBlock(final CodeBlockContent content) {
+        HtmlType htmlType = HtmlType.findByBlockType(content.getBlockType());
+        String language = content.getLanguage();
+        String rawText = content.getRawText();
+
+        String startTag = htmlType.getStartTag()
+                .replace("${language}", language);
+
+        return startTag + rawText + htmlType.getEndTag();
+    }
+
+    public String renderList(final List<NormalContent> contents) {
+        StringBuilder result = new StringBuilder();
+        NormalContent firstContent = contents.get(0);
+        NormalContent endContent = contents.get(contents.size() - 1);
+
+        addFirstHtmlType(firstContent, result);
+        addInnerHtmlType(contents, result);
+        addEndHtmlType(endContent, result);
+
+        return String.valueOf(result);
+    }
+
+    private void addFirstHtmlType(final NormalContent content, final StringBuilder result) {
+        HtmlType htmlType = HtmlType.findByBlockType(content.getBlockType());
+        result.append(htmlType.getStartTag());
+    }
+
+    private void addInnerHtmlType(final List<NormalContent> contents, final StringBuilder result) {
+        int contentSize = contents.size();
+
+        for (int i = 0; i < contentSize - 1; i++) {
+            NormalContent currentContent = contents.get(i);
+            NormalContent nextContent = contents.get(i + 1);
+
+            result.append(renderLine(currentContent, nextContent));
+        }
+    }
+
+    private String renderLine(final NormalContent content, NormalContent nextContent) {
+        // TODO: style 적용
+        String line = HtmlType.LIST.getStartTag() + content.getRawText() + HtmlType.LIST.getEndTag();
+
+        if (content.getDepth() > nextContent.getDepth()) {
+            HtmlType htmlType = HtmlType.findByBlockType(content.getBlockType());
+            return line + htmlType.getEndTag();
+        }
+
+        if (content.getDepth() == nextContent.getDepth()) {
+            if (content.getBlockType().equals(nextContent.getBlockType())) {
+                return line;
+            }
+            HtmlType htmlType = HtmlType.findByBlockType(content.getBlockType());
+            HtmlType nextHtmlType = HtmlType.findByBlockType(nextContent.getBlockType());
+            return line + htmlType.getEndTag() + nextHtmlType.getStartTag();
+        }
+
+        HtmlType nextHtmlType = HtmlType.findByBlockType(nextContent.getBlockType());
+        return line + nextHtmlType.getStartTag();
+    }
+
+    private void addEndHtmlType(final NormalContent content, final StringBuilder result) {
+        HtmlType htmlType = HtmlType.findByBlockType(content.getBlockType());
+        String line = HtmlType.LIST.getStartTag() + content.getRawText() + HtmlType.LIST.getEndTag() + htmlType.getEndTag();
+        result.append(line);
+    }
+
+    public String renderImage(final ImageContent content) {
+        HtmlType htmlType = HtmlType.findByBlockType(content.getBlockType());
+        String caption = content.getCaption();
+        String url = content.getUrl();
+
+        String startTag = htmlType.getStartTag()
+                .replace("caption", caption)
+                .replace("url", url);
+
+        return startTag + htmlType.getEndTag();
+    }
+}

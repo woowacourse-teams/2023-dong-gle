989db0fcde68cffd6fb7edc3eea73772613bc579
echo <echo.backend@gmail.com>
2023-07-19 16:36:22 +0900

    feat: Medium API 서비스 클래스 구현
    Co-authored-by: HubCreator <chocomilk8604@gmail.com>

---
+++ backend/src/main/java/org/donggle/backend/application/service/medium/dto/MediumUserData.java
@@ -0,0 +1,10 @@
+package org.donggle.backend.application.service.medium.dto;
+
+public record MediumUserData(
+        String id,
+        String username,
+        String name,
+        String url,
+        String imageUrl
+) {
+}
---
+++ backend/src/main/java/org/donggle/backend/application/service/medium/dto/MediumUserResponse.java
@@ -0,0 +1,4 @@
+package org.donggle.backend.application.service.medium.dto;
+
+public record MediumUserResponse(MediumUserData data) {
+}

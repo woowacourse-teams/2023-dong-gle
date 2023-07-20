26b5692917af343695aca510e613222ea606314e
ezzanzzan <cjsdmswjd821@naver.com>
2023-07-20 11:59:46 +0900

    feat: 글 정보 조회 service 기능 구현
    Co-authored-by: ingpyo <juninpyo726@naver.com>

--- backend/src/main/java/org/donggle/backend/application/WritingService.java
+++ backend/src/main/java/org/donggle/backend/application/WritingService.java
@@ -2,10 +2,12 @@ package org.donggle.backend.application;
 
 import lombok.RequiredArgsConstructor;
 import org.donggle.backend.application.repository.BlockRepository;
+import org.donggle.backend.application.repository.BlogWritingRepository;
 import org.donggle.backend.application.repository.ContentRepository;
 import org.donggle.backend.application.repository.MemberRepository;
 import org.donggle.backend.application.repository.WritingRepository;
 import org.donggle.backend.domain.Block;
+import org.donggle.backend.domain.BlogWriting;
 import org.donggle.backend.domain.Member;
 import org.donggle.backend.domain.Writing;
 import org.donggle.backend.domain.content.Content;
@@ -29,6 +31,7 @@ public class WritingService {
     private final ContentRepository contentRepository;
     private final BlockRepository blockRepository;
     private final WritingRepository writingRepository;
+    private final BlogWritingRepository blogWritingRepository;
 
     @Transactional
     public Long uploadMarkDownFile(final Long memberId, final String originalFilename, final String totalText) {
@@ -61,6 +64,15 @@ public class WritingService {
     }
 
     public WritingPropertiesResponse findWritingProperties(final Long memberId, final Long writingId) {
-        return null;
+        // TODO : authentication 후 member 객체 가져오도록 수정 후 검증 로직 추가
+        final Writing writing = writingRepository.findById(writingId)
+                .orElseThrow(() -> new WritingNotFoundException(writingId));
+        List<BlogWriting> blogWritings = blogWritingRepository.findByWritingId(writingId);
+
+        List<String> blogNames = blogWritings.stream()
+                .map(blogWriting -> blogWriting.getBlog().getName())
+                .toList();
+
+        return new WritingPropertiesResponse(writing.getCreatedAt(), writing.isPublished(), writing.getPublishedAt(), blogNames);
     }
 }
--- backend/src/main/java/org/donggle/backend/application/repository/BlogWritingRepository.java
+++ backend/src/main/java/org/donggle/backend/application/repository/BlogWritingRepository.java
@@ -3,5 +3,8 @@ package org.donggle.backend.application.repository;
 import org.donggle.backend.domain.BlogWriting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BlogWritingRepository extends JpaRepository<BlogWriting, Long> {
+    List<BlogWriting> findByWritingId(Long writingId);
 }

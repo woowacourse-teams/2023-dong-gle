20da2225a71d0887e7e63b750af004cbc5f9c0b6
echo <echo.backend@gmail.com>
2023-07-20 13:24:23 +0900

    refactor: Blog 내부 타입 Enum 변경
    Co-authored-by: HubCreator <chocomilk8604@gmail.com>

--- backend/src/main/java/org/donggle/backend/domain/Blog.java
+++ backend/src/main/java/org/donggle/backend/domain/Blog.java
@@ -1,11 +1,6 @@
 package org.donggle.backend.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +12,6 @@ public class Blog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull
-    @Column(length = 20)
-    private String name;
+    @Enumerated(value = EnumType.STRING)
+    private BlogType blogType;
 }

a7ec2fd8bf08d5868d1d40ae367377dfc7e077e3
yogjin <youngjin988@gmail.com>
2023-07-27 01:54:07 +0900

    test: `WritingTable` 스토리 추가

---
+++ frontend/src/components/WritingTable/WritingTable.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react';
+import WritingTable from './WritingTable';
+import {
+  StoryContainer,
+  StoryItemContainer,
+  StoryItemContainerRow,
+  StoryItemTitle,
+} from 'styles/storybook';
+
+const meta = {
+  title: 'WritingTable',
+  component: WritingTable,
+  // args: {},
+  // argTypes: {},
+} satisfies Meta<typeof WritingTable>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Playground: Story = {
+  render: ({ categoryId }) => {
+    return (
+      <StoryContainer>
+        <StoryItemContainer style={{ width: '800px' }}>
+          <StoryItemTitle>기본</StoryItemTitle>
+          <WritingTable categoryId={categoryId} />
+        </StoryItemContainer>
+      </StoryContainer>
+    );
+  },
+};

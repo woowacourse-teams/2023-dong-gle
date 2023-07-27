efb3519b5aa1c7e1073a822ee0399491533861c5
nangkyeonglim <nangkyeonglim@gmail.com>
2023-07-28 02:23:04 +0900

    test: `PublishingPropertySection` 스토리 작성

---
+++ frontend/src/components/PublishingPropertySection/PublishingPropertySection.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { StoryContainer } from 'styles/storybook';
+import PublishingPropertySection from './PublishingPropertySection';
+
+const meta: Meta<typeof PublishingPropertySection> = {
+  title: 'publishing/PublishingPropertySection',
+  component: PublishingPropertySection,
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  render: () => (
+    <StoryContainer>
+      <PublishingPropertySection changeTab={() => () => {}} />
+    </StoryContainer>
+  ),
+};

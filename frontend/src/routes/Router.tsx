a360094cbee048f8ce0177329e65dc13c9f7e0db
yogjin <youngjin988@gmail.com>
2023-07-27 11:14:48 +0900

    feat: `WritingTablePage` routes 추가

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from 'pages/Layout/Layout';
 import WritingPage from 'pages/WritingPage/WritingPage';
+import WritingTablePage from 'pages/WritingTablePage/WritingTablePage';
 
 export const Router = () => {
   const browserRouter = createBrowserRouter([
@@ -11,6 +12,10 @@ export const Router = () => {
         {
           path: '/writing/:writingId',
           element: <WritingPage />,
+        },
+        {
+          path: '/writings/:categoryId',
+          element: <WritingTablePage />,
         },
       ],
     },

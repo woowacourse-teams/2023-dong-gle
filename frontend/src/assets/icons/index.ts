c1f218c0a8efda2c706766e35313f15eb4d12b9a
nangkyeonglim <nangkyeonglim@gmail.com>
2023-07-27 23:34:37 +0900

    feat: 태그, 왼쪽 화살표 svg 아이콘 추가

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -4,3 +4,5 @@ export { ReactComponent as PlusCircleIcon } from './plus-circle.svg';
 export { ReactComponent as SearchIcon } from './search.svg';
 export { ReactComponent as StatsIcon } from './stats.svg';
 export { ReactComponent as CheckSymbol } from './check-symbol.svg';
+export { ReactComponent as TagIcon } from './tag.svg';
+export { ReactComponent as LeftArrowHeadIcon } from './left-arrow-head.svg';

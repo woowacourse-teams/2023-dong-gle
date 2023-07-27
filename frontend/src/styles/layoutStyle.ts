cdcccf0028f6d97311ea1393db6d89d7d7e63255
yogjin <youngjin988@gmail.com>
2023-07-28 01:36:35 +0900

    !design: sidebar open여부에 따라 다른 중간 페이지 width 구하는 `genMainPageWidth` 함수 추가

--- frontend/src/styles/layoutStyle.ts
+++ frontend/src/styles/layoutStyle.ts
@@ -1,4 +1,4 @@
-import { css } from 'styled-components';
+import { RuleSet, css } from 'styled-components';
 import { theme } from './theme';
 
 export const LAYOUT_STYLE = {
@@ -18,3 +18,27 @@ export const sidebarStyle = css`
   padding: 2rem;
   flex: 0 0 ${SIDEBAR_STYLE.width};
 `;
+
+export const genMainPageWidth = (
+  isLeftSidebarOpen: boolean,
+  isRightSidebarOpen: boolean,
+): RuleSet<object> => {
+  if (isLeftSidebarOpen === false && isRightSidebarOpen === false)
+    return css`
+      max-width: calc(100vw - (${LAYOUT_STYLE.padding}) * 2);
+    `;
+  if (
+    (isLeftSidebarOpen === false && isRightSidebarOpen === true) ||
+    (isLeftSidebarOpen === true && isRightSidebarOpen === false)
+  )
+    return css`
+      max-width: calc(
+        100vw - (${LAYOUT_STYLE.padding}) * 2 - (${SIDEBAR_STYLE.width}) - (${LAYOUT_STYLE.gap})
+      );
+    `;
+  return css`
+    max-width: calc(
+      100vw - (${SIDEBAR_STYLE.width} + ${LAYOUT_STYLE.padding} + ${LAYOUT_STYLE.gap}) * 2
+    );
+  `;
+};

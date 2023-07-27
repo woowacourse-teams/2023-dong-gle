4a11c6d8bd911fb9f6e1da072eb196d0afcd8682
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-07-27 20:39:47 +0900

    feat: 카테고리 추가, 카테고리 목록 조회, 카테고리 글 목록 조회 fetcher 구현

---
+++ frontend/src/apis/category.ts
@@ -0,0 +1,13 @@
+import { categoryURL } from 'constants/apis/url';
+import { http } from './fetch';
+import { AddCategoriesRequest } from 'types/apis/category';
+
+// POST: 카테고리 추가
+export const addCategory = (body: AddCategoriesRequest) =>
+  http.post(categoryURL, { body: JSON.stringify(body) });
+
+// GET: 카테고리 목록 조회
+export const getCategories = () => http.get(categoryURL);
+
+// GET: 카테고리 글 목록 조회
+export const WritingsInCategory = (categoryId: number) => http.get(`${categoryURL}/${categoryId}`);

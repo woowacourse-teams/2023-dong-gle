95b9416d36c357269ae0796e83e6466d5d6bfc55
yogjin <youngjin988@gmail.com>
2023-07-27 13:44:15 +0900

    feat: `WritingTable` 내부 배포한 블로그 로고 표시 기능 추가

--- frontend/src/components/WritingTable/WritingTable.tsx
+++ frontend/src/components/WritingTable/WritingTable.tsx
@@ -1,6 +1,9 @@
+import { MediumLogoIcon, TistoryLogoIcon } from 'assets/icons';
+import { BLOG_LIST } from 'constants/blog';
 import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 import { Writing } from 'types/apis/writings';
+import { Blog } from 'types/domain';
 import { dateFormatter } from 'utils/date';
 
 type Props = { writings: Writing[] };
@@ -9,6 +12,15 @@ const WritingTable = ({ writings }: Props) => {
   const navigate = useNavigate();
 
   const goWritingPage = (writingId: number) => navigate(`/writing/${writingId}`);
+
+  const getPublishedToLogoIcon = (blogName: Blog) => {
+    switch (blogName) {
+      case 'MEDIUM':
+        return <MediumLogoIcon width='2.4rem' />;
+      case 'TISTORY':
+        return <TistoryLogoIcon width='2.4rem' />;
+    }
+  };
 
   return (
     <>
@@ -21,7 +33,13 @@ const WritingTable = ({ writings }: Props) => {
         {writings.map((writing) => (
           <tr key={writing.id} onClick={() => goWritingPage(writing.id)}>
             <td>{writing.title}</td>
-            <td>{writing.publishedDetails[0].blogName}</td>
+            <td>
+              <div className='publishedTo'>
+                {writing.publishedDetails.map((publishedDetail) =>
+                  getPublishedToLogoIcon(publishedDetail.blogName),
+                )}
+              </div>
+            </td>
             <td>{dateFormatter(writing.createdAt, 'YYYY.MM.DD.')}</td>
           </tr>
         ))}
@@ -35,12 +53,18 @@ export default WritingTable;
 const S = {
   WritingTableContainer: styled.table`
     width: 100%;
-    border-left: none;
     text-align: left;
     font-size: 1.4rem;
 
     th {
       color: ${({ theme }) => theme.color.gray7};
+    }
+
+    td {
+      .publishedTo {
+        display: flex;
+        gap: 0.8rem;
+      }
     }
 
     th,

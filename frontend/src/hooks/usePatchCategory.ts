186e5f23100fd624930dd301f0a7a6199b274209
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-07-28 17:30:46 +0900

    feat: `usePatchCategory` 훅 구현

---
+++ frontend/src/hooks/usePatchCategory.ts
@@ -0,0 +1,26 @@
+import { patchCategory } from 'apis/category';
+import useMutation from './@common/useMutation';
+
+export const usePatchCategory = () => {
+  const { mutateQuery } = useMutation({
+    fetcher: patchCategory,
+  });
+
+  const renameCategory = async (categoryId: number, newCategoryName: string) => {
+    const body = {
+      categoryName: newCategoryName,
+    };
+
+    await mutateQuery({ categoryId, body });
+  };
+
+  const moveCategory = async (categoryId: number, nextCategoryId: number) => {
+    const body = {
+      nextCategoryId,
+    };
+
+    await mutateQuery({ categoryId, body });
+  };
+
+  return { renameCategory, moveCategory };
+};

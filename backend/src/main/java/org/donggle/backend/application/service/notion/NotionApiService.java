568c0dce1ecddda1ab374ffdba41b564771faf4d
echo724 <echo.backend@gmail.com>
2023-07-28 17:48:09 +0900

    refactor: `NotionApiService` 리팩토링
    Co-authored-by: ingpyo <juninpyo726@gmail.com>

--- backend/src/main/java/org/donggle/backend/application/service/notion/NotionApiService.java
+++ backend/src/main/java/org/donggle/backend/application/service/notion/NotionApiService.java
@@ -5,11 +5,17 @@ import org.donggle.backend.application.service.notion.exception.NotionForbiddenE
 import org.donggle.backend.application.service.notion.exception.NotionInvalidRequestException;
 import org.donggle.backend.application.service.notion.exception.NotionNotFoundException;
 import org.donggle.backend.application.service.notion.exception.NotionUnAuthorizedException;
+import org.donggle.backend.domain.parser.notion.NotionBlockType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 
 @Component
@@ -25,15 +31,20 @@ public class NotionApiService {
     public static final int FORBIDDEN = 403;
     public static final int NOT_FOUND = 404;
     private static final String NOTION_URL = "https://api.notion.com/v1";
+
     private final String notionToken;
     private final WebClient webClient;
 
-    public NotionApiService(@Value("${notion_token_temp}") String notionToken) {
+    public NotionApiService(@Value("${notion_token_temp}") final String notionToken) {
         this.notionToken = notionToken;
         this.webClient = WebClient.create(NOTION_URL);
     }
 
-    public JsonNode retrieveParentBlock(String parentBlockId) {
+    public NotionBlockNode retrieveParentBlockNode(final String parentBlockId) {
+        return new NotionBlockNode(retrieveBlock(parentBlockId), -1);
+    }
+
+    public JsonNode retrieveBlock(final String parentBlockId) {
         return retrieveData(parentBlockId, BLOCK_URI);
     }
 
@@ -44,6 +55,7 @@ public class NotionApiService {
                 .header(NOTION_VERSION, API_VERSION)
                 .retrieve()
                 .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> handle4xxException(clientResponse.statusCode().value()))
+                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new RuntimeException(clientResponse.toString())))
                 .bodyToMono(JsonNode.class)
                 .block();
     }
@@ -58,7 +70,43 @@ public class NotionApiService {
         };
     }
 
-    public JsonNode retrieveChildrenBlocks(String blockId) {
-        return retrieveData(blockId, CHILD_BLOCK_URI);
+    public List<NotionBlockNode> retrieveBodyBlockNodes(final NotionBlockNode parentNotionBlock) {
+        final List<NotionBlockNode> bodyBlockNodes = new ArrayList<>();
+        final Deque<NotionBlockNode> notionBlockNodeDeque = new ArrayDeque<>();
+        notionBlockNodeDeque.addFirst(parentNotionBlock);
+
+        while (!notionBlockNodeDeque.isEmpty()) {
+            final NotionBlockNode notionBlockNode = notionBlockNodeDeque.removeFirst();
+            processNotionBlockNode(notionBlockNode, bodyBlockNodes, notionBlockNodeDeque);
+        }
+
+        return bodyBlockNodes;
+    }
+
+    private void processNotionBlockNode(final NotionBlockNode notionBlockNode, final List<NotionBlockNode> bodyBlockNodes, final Deque<NotionBlockNode> notionBlockNodeDeque) {
+        bodyBlockNodes.add(notionBlockNode);
+
+        if (notionBlockNode.hasChildren()) {
+            final List<JsonNode> childrenBlocks = retrieveChildrenBlocks(notionBlockNode.getId());
+            processChildrenBlocks(childrenBlocks, notionBlockNode, notionBlockNodeDeque);
+        }
+    }
+
+    private List<JsonNode> retrieveChildrenBlocks(final String blockId) {
+        final List<JsonNode> childrenBlocks = new ArrayList<>();
+        retrieveData(blockId, CHILD_BLOCK_URI).withArray("results").elements().forEachRemaining(childrenBlocks::add);
+        return childrenBlocks;
+    }
+
+    private void processChildrenBlocks(final List<JsonNode> childrenBlocks, final NotionBlockNode notionBlockNode, final Deque<NotionBlockNode> notionBlockNodeDeque) {
+        for (int i = childrenBlocks.size() - 1; i >= 0; i--) {
+            final JsonNode childBlock = childrenBlocks.get(i);
+            if (notionBlockNode.getBlockType() == NotionBlockType.COLUMN || notionBlockNode.getBlockType() == NotionBlockType.COLUMN_LIST) {
+                notionBlockNodeDeque.addFirst(new NotionBlockNode(childBlock, 0));
+                continue;
+            }
+            notionBlockNodeDeque.addFirst(new NotionBlockNode(childBlock, notionBlockNode.depth() + 1));
+
+        }
     }
 }

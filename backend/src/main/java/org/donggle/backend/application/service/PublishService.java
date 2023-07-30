c6ddc354add33876c1ceea1a1fe93d29b45025f2
ingpyo <juninpyo726@naver.com>
2023-07-30 16:03:50 +0900

    feat: PublishService 기능 구현

--- backend/src/main/java/org/donggle/backend/application/service/PublishService.java
+++ backend/src/main/java/org/donggle/backend/application/service/PublishService.java
@@ -9,6 +9,9 @@ import org.donggle.backend.application.service.medium.MediumApiService;
 import org.donggle.backend.application.service.medium.dto.request.MediumPublishRequest;
 import org.donggle.backend.application.service.medium.dto.response.MediumPublishResponse;
 import org.donggle.backend.application.service.request.PublishRequest;
+import org.donggle.backend.application.service.tistory.TistoryApiService;
+import org.donggle.backend.application.service.tistory.request.TistoryPublishRequest;
+import org.donggle.backend.application.service.tistory.response.TistoryPublishWritingResponse;
 import org.donggle.backend.domain.blog.Blog;
 import org.donggle.backend.domain.blog.BlogType;
 import org.donggle.backend.domain.blog.BlogWriting;
@@ -30,6 +33,7 @@ public class PublishService {
     private final BlockRepository blockRepository;
     private final BlogWritingRepository blogWritingRepository;
     private final MediumApiService mediumApiService;
+    private final TistoryApiService tistoryApiService;
 
     public void publishWriting(final Long memberId, final Long writingId, final PublishRequest publishRequest) {
         final String blogName = publishRequest.publishTo();
@@ -53,7 +57,15 @@ public class PublishService {
                 blogWritingRepository.save(blogWriting);
             }
             case TISTORY -> {
-                //TODO : TISTORY API 연동
+                final TistoryPublishRequest request = TistoryPublishRequest.builder()
+                        .access_token("tistoryToken")
+                        .blogName("jeoninpyo726")
+                        .title(writing.getTitle())
+                        .content(content)
+                        .build();
+                final TistoryPublishWritingResponse response = tistoryApiService.publishContent(request);
+                final BlogWriting blogWriting = new BlogWriting(blog, writing, response.tistory().item().getDateTime());
+                blogWritingRepository.save(blogWriting);
             }
         }
     }

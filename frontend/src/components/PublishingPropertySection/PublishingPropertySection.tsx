ed5e315d98c504872a3d92f918c946454bf3de1b
nangkyeonglim <nangkyeonglim@gmail.com>
2023-08-01 01:06:48 +0900

    feat: 발행정보와 함께 발행가능하도록 로직 구현

--- frontend/src/components/PublishingPropertySection/PublishingPropertySection.tsx
+++ frontend/src/components/PublishingPropertySection/PublishingPropertySection.tsx
@@ -1,19 +1,25 @@
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 import TagInput from '../@common/TagInput/TagInput';
 import Button from '../@common/Button/Button';
 import { LeftArrowHeadIcon, TagIcon } from 'assets/icons';
 import { slide } from 'styles/animation';
+import { TAB_KEYS } from 'components/WritingSideBar/WritingSideBar';
+import { usePublishingPropertySection } from './usePublishingPropertySection';
+import type { Blog } from 'types/domain';
 
 type Props = {
-  changeTab: (index: number) => () => void;
+  writingId: number;
+  publishTo: Blog;
+  changeCurrentTab: (key: number) => void;
 };
 
-const PublishingPropertySection = ({ changeTab }: Props) => {
+const PublishingPropertySection = ({ writingId, publishTo, changeCurrentTab }: Props) => {
+  const { setTags, publishWritingToBlog } = usePublishingPropertySection();
+
   return (
-    <S.PublishingPropertySection>
+    <S.PublishingPropertySection blog={publishTo}>
       <S.SectionHeader>
-        {/* TODO: changeTab에 index 상수화 */}
-        <button onClick={changeTab(1)}>
+        <button onClick={() => changeCurrentTab(TAB_KEYS.Publishing)}>
           <LeftArrowHeadIcon width={14} height={14} />
         </button>
         Publishing Properties
@@ -25,11 +31,15 @@ const PublishingPropertySection = ({ changeTab }: Props) => {
             Tags
           </S.PropertyName>
           <div>
-            <TagInput />
+            <TagInput onChangeTags={setTags} />
           </div>
         </S.PropertyRow>
       </S.Properties>
-      <Button block variant='secondary'>
+      <Button
+        block
+        variant='secondary'
+        onClick={() => publishWritingToBlog({ writingId, publishTo })}
+      >
         Publish
       </Button>
     </S.PublishingPropertySection>
@@ -39,11 +49,22 @@ const PublishingPropertySection = ({ changeTab }: Props) => {
 export default PublishingPropertySection;
 
 const S = {
-  PublishingPropertySection: styled.section`
+  PublishingPropertySection: styled.section<{ blog: Blog }>`
     display: flex;
     flex-direction: column;
     gap: 2rem;
     animation: ${slide} 0.5s;
+
+    ${({ theme, blog }) => css`
+      & > button {
+        outline-color: ${theme.color[blog.toLowerCase()]};
+        background-color: ${theme.color[blog.toLowerCase()]};
+
+        &:hover {
+          background-color: ${theme.color[blog.toLowerCase()]};
+        }
+      }
+    `};
   `,
   SectionHeader: styled.h1`
     display: flex;
@@ -53,7 +74,7 @@ const S = {
     line-height: 1.5rem;
   `,
   Properties: styled.div`
-    padding-left: 0.9rem;
+    padding: 0 0 1rem 0.9rem;
   `,
   PropertyRow: styled.div`
     display: flex;
@@ -64,7 +85,7 @@ const S = {
     align-items: center;
     flex-shrink: 0;
     width: 9.5rem;
-    height: 100%;
+    height: 2.3rem;
     color: ${({ theme }) => theme.color.gray8};
     font-size: 1.3rem;
     font-weight: 600;

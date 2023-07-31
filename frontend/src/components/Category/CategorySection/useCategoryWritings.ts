6243f4d9b324d4e19776bf44a231be165addab01
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-01 04:00:24 +0900

    chore: `useCategoryDetail` 훅 이름 변경

--- frontend/src/components/Category/CategorySection/useCategoryWritings.ts
+++ frontend/src/components/Category/CategorySection/useCategoryWritings.ts
@@ -1,16 +1,16 @@
-import { GetCategoryDetailsResponse } from 'types/apis/category';
+import { GetCategoryDetailResponse } from 'types/apis/category';
 import { useGetQuery } from '../../../hooks/@common/useGetQuery';
 import { getWritingsInCategory } from 'apis/category';
 import { useCallback, useEffect, useState } from 'react';
 
-export const useCategoryDetail = () => {
+export const useCategoryWritings = () => {
   const [categoryId, setCategoryId] = useState<number | null>(null);
 
   const fetcher = useCallback(() => {
     return categoryId ? getWritingsInCategory(categoryId) : Promise.reject('No Category ID');
   }, [categoryId]);
 
-  const { data, getData } = useGetQuery<GetCategoryDetailsResponse>({
+  const { data, getData } = useGetQuery<GetCategoryDetailResponse>({
     fetcher,
   });
 
@@ -18,9 +18,13 @@ export const useCategoryDetail = () => {
     getData();
   }, [categoryId]);
 
-  const getWritings = (categoryId: number) => {
+  const getWritingsRequest = (categoryId: number) => {
     setCategoryId(categoryId);
   };
 
-  return { getWritings, categoryId: data?.id, writings: data?.writings };
+  return {
+    getWritingsRequest,
+    categoryId: data ? data.id : null,
+    writings: data ? data.writings : null,
+  };
 };

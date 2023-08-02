3c2055e3956210388c25d293c52908ac8d30fa54
Eun-chan Cho <echo.backend@gmail.com>
2023-08-02 11:45:04 +0900

    [BE] test: NotionParser 테스트코드 추가 (#141)
    * test: NotionParser 테스트코드 추가
    
    Co-authored-by: ingpyo <juninpyo726@naver.com>
    
    * style: 리뷰 반영
    
    Co-authored-by: ingpyo <juninpyo726@naver.com>
    
    ---------
    
    Co-authored-by: ingpyo <juninpyo726@naver.com>

---
+++ backend/src/test/java/org/donggle/backend/domain/parser/notion/NotionBlockTypeTest.java
@@ -0,0 +1,32 @@
+package org.donggle.backend.domain.parser.notion;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NotionBlockTypeTest {
+    @Test
+    @DisplayName("NotionBlockType을 파싱하는 테스트")
+    void findNotionBlockType() {
+        //given
+
+        //when
+        final NotionBlockType blockType = NotionBlockType.findBlockType("heading_1");
+
+        //then
+        assertThat(blockType).isEqualTo(NotionBlockType.HEADING_1);
+    }
+
+    @Test
+    @DisplayName("없는 NotionBlockType을 찾을 경우 예외를 던지는 테스트")
+    void findNotionBlockTypeError() {
+        //given
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> NotionBlockType.findBlockType("존재 안하는 블럭")).isInstanceOf(IllegalArgumentException.class);
+    }
+}

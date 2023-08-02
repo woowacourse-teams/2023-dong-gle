03a1f7c7845f77ff4fa8762b769acbfb676974ad
yogjin <youngjin988@gmail.com>
2023-08-02 22:50:52 +0900

    feat: `fetch` 추상화 함수 파라미터 option에 json body 쉽게 넣는 필드 추가

--- frontend/src/apis/fetch.ts
+++ frontend/src/apis/fetch.ts
@@ -1,29 +1,62 @@
+type Option = {
+  json?: unknown;
+} & RequestInit;
+
+const getOptions = (option: Option): RequestInit => {
+  let parsedOption: RequestInit = { ...option };
+
+  if (option.json) {
+    parsedOption = {
+      ...parsedOption,
+      body: JSON.stringify(option.json),
+      headers: {
+        ...parsedOption.headers,
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+
+  return parsedOption;
+};
+
 export const http = {
-  get(endpoint: RequestInfo | URL, option?: RequestInit) {
-    return fetch(endpoint, {
-      method: 'GET',
-      ...option,
-    });
+  get(endpoint: RequestInfo | URL, option?: Option) {
+    return fetch(
+      endpoint,
+      getOptions({
+        method: 'GET',
+        ...option,
+      }),
+    );
   },
 
-  post(endpoint: RequestInfo | URL, option?: RequestInit) {
-    return fetch(endpoint, {
-      method: 'POST',
-      ...option,
-    });
+  post(endpoint: RequestInfo | URL, option?: Option) {
+    return fetch(
+      endpoint,
+      getOptions({
+        method: 'POST',
+        ...option,
+      }),
+    );
   },
 
-  patch(endpoint: RequestInfo | URL, option?: RequestInit) {
-    return fetch(endpoint, {
-      method: 'PATCH',
-      ...option,
-    });
+  patch(endpoint: RequestInfo | URL, option?: Option) {
+    return fetch(
+      endpoint,
+      getOptions({
+        method: 'PATCH',
+        ...option,
+      }),
+    );
   },
 
-  delete(endpoint: RequestInfo | URL, option?: RequestInit) {
-    return fetch(endpoint, {
-      method: 'DELETE',
-      ...option,
-    });
+  delete(endpoint: RequestInfo | URL, option?: Option) {
+    return fetch(
+      endpoint,
+      getOptions({
+        method: 'DELETE',
+        ...option,
+      }),
+    );
   },
 };

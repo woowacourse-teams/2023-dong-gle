c88873d9e537c77d4be2b0f08cfa4dcbb8f12e8d
yogjin <youngjin988@gmail.com>
2023-08-02 20:37:49 +0900

    refactor: 기본 fetch option header 제거

--- frontend/src/apis/fetch.ts
+++ frontend/src/apis/fetch.ts
@@ -1,32 +1,26 @@
-const FETCH_DEFAULT_OPTION = {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-} as const;
-
 export const http = {
-  get(endpoint: RequestInfo | URL, option: RequestInit = FETCH_DEFAULT_OPTION) {
+  get(endpoint: RequestInfo | URL, option?: RequestInit) {
     return fetch(endpoint, {
       method: 'GET',
       ...option,
     });
   },
 
-  post(endpoint: RequestInfo | URL, option: RequestInit = FETCH_DEFAULT_OPTION) {
+  post(endpoint: RequestInfo | URL, option?: RequestInit) {
     return fetch(endpoint, {
       method: 'POST',
       ...option,
     });
   },
 
-  patch(endpoint: RequestInfo | URL, option: RequestInit = FETCH_DEFAULT_OPTION) {
+  patch(endpoint: RequestInfo | URL, option?: RequestInit) {
     return fetch(endpoint, {
       method: 'PATCH',
       ...option,
     });
   },
 
-  delete(endpoint: RequestInfo | URL, option: RequestInit = FETCH_DEFAULT_OPTION) {
+  delete(endpoint: RequestInfo | URL, option?: RequestInit) {
     return fetch(endpoint, {
       method: 'DELETE',
       ...option,

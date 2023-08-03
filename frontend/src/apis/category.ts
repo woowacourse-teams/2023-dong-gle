771666c52f0f8323f7e2d7286fcc1f7b14d506b7
nangkyeonglim <nangkyeonglim@gmail.com>
2023-08-03 15:49:38 +0900

    fix: 카테고리 API에 headers content type 추가

--- frontend/src/apis/category.ts
+++ frontend/src/apis/category.ts
@@ -4,7 +4,12 @@ import { AddCategoriesRequest, PatchCategoryArgs } from 'types/apis/category';
 
 // POST: 카테고리 추가
 export const addCategory = (body: AddCategoriesRequest) =>
-  http.post(categoryURL, { body: JSON.stringify(body) });
+  http.post(categoryURL, {
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
 // GET: 카테고리 목록 조회
 export const getCategories = () => http.get(categoryURL);
@@ -17,6 +22,9 @@ export const getWritingsInCategory = (categoryId: number | null) =>
 export const patchCategory = ({ categoryId, body }: PatchCategoryArgs) =>
   http.patch(`${categoryURL}/${categoryId}`, {
     body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
 
 // DELETE: 카테고리 삭제

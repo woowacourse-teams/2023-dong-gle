6588a5534e79bad338e0283c97b086dda3ab4acd
yogjin <youngjin988@gmail.com>
2023-08-08 01:14:38 +0900

    feat: `fetchAPI` 함수 추가

--- frontend/src/apis/fetch.ts
+++ frontend/src/apis/fetch.ts
@@ -1,8 +1,33 @@
+import { HttpError, handleHttpError } from 'types/apis/http';
+
 type Option = {
   json?: unknown;
 } & RequestInit;
 
-const getOptions = (option: Option): RequestInit => {
+const fetchAPI = async (endpoint: RequestInfo | URL, option: Option) => {
+  try {
+    const response = await fetch(endpoint, parseOption(option));
+
+    if (!response.ok) {
+      handleHttpError(response);
+    }
+
+    const contentType = response.headers.get('content-type');
+
+    if (!contentType && response.ok) return response;
+
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    // httpError 면 던져서 fetch API 사용하는 쪽에서 핸들링
+    if (error instanceof HttpError) {
+      throw error;
+    }
+    alert(error); // httpError가 아니면 여기서 바로 alert
+  }
+};
+
+const parseOption = (option: Option): RequestInit => {
   let parsedOption: RequestInit = { ...option };
 
   if (option.json) {
@@ -21,42 +46,30 @@ const getOptions = (option: Option): RequestInit => {
 
 export const http = {
   get(endpoint: RequestInfo | URL, option?: Option) {
-    return fetch(
-      endpoint,
-      getOptions({
-        method: 'GET',
-        ...option,
-      }),
-    );
+    return fetchAPI(endpoint, {
+      method: 'GET',
+      ...option,
+    });
   },
 
   post(endpoint: RequestInfo | URL, option?: Option) {
-    return fetch(
-      endpoint,
-      getOptions({
-        method: 'POST',
-        ...option,
-      }),
-    );
+    return fetchAPI(endpoint, {
+      method: 'POST',
+      ...option,
+    });
   },
 
   patch(endpoint: RequestInfo | URL, option?: Option) {
-    return fetch(
-      endpoint,
-      getOptions({
-        method: 'PATCH',
-        ...option,
-      }),
-    );
+    return fetchAPI(endpoint, {
+      method: 'PATCH',
+      ...option,
+    });
   },
 
   delete(endpoint: RequestInfo | URL, option?: Option) {
-    return fetch(
-      endpoint,
-      getOptions({
-        method: 'DELETE',
-        ...option,
-      }),
-    );
+    return fetchAPI(endpoint, {
+      method: 'DELETE',
+      ...option,
+    });
   },
 };

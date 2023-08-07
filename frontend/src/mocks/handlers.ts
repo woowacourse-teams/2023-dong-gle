d51454c1d81ffe828829593ca76d79d6faa45584
yogjin <youngjin988@gmail.com>
2023-08-08 00:48:53 +0900

    feat: 에러 mocking 핸들러 추가

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,4 +1,5 @@
 import { categoryHandlers } from './handlers/category';
+import { errorHandlers } from './handlers/error';
 import { writingHandlers } from './handlers/writing';
 
-export const handlers = [...writingHandlers, ...categoryHandlers];
+export const handlers = [...writingHandlers, ...categoryHandlers, ...errorHandlers];

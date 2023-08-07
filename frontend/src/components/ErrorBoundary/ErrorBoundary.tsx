2aef35b9dfc5977b1c9830baca603cc15a597260
yogjin <youngjin988@gmail.com>
2023-08-08 01:15:17 +0900

    feat: `ErrorBoundary` 뼈대 추가

---
+++ frontend/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,45 @@
+import { HttpStatus } from 'constants/apis/http';
+import React, { ErrorInfo, ReactNode } from 'react';
+import { HttpError } from 'types/apis/http';
+
+type Props = {
+  children: ReactNode;
+  fallback?: ReactNode;
+  onReset?: VoidFunction;
+};
+
+type State = {
+  error: Error | null;
+};
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  // getDerivedStateFrom~ → render → componentDid~ 순서로 동작
+  constructor(props: Props) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  // 다음 렌더링에서 폴백 UI가 보이도록 상태를 업데이트
+  static getDerivedStateFromError(error: Error) {
+    return { error: error || null };
+  }
+
+  // You can render any custom fallback UI
+  render() {
+    if (this.state.error) {
+      const error = this.state.error;
+      if (error instanceof HttpError) {
+        if (error.statusCode === HttpStatus.UNAUTHORIZED) {
+          alert('로그인 해주세요');
+        }
+      }
+    }
+
+    return this.props.children;
+  }
+
+  // 에러 리포팅 서비스에 에러를 기록
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    // TODO::Sentry
+  }
+}

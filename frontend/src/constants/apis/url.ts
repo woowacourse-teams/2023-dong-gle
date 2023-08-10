a444f7a4fd8837356612feb74f68d56bcd81e430
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-10 17:31:38 +0900

    feat: `oauthRedirectURL`, `oauthLoginURL` 추가

--- frontend/src/constants/apis/url.ts
+++ frontend/src/constants/apis/url.ts
@@ -1,3 +1,13 @@
+import { OauthPlatform } from 'types/apis/login';
+
 export const baseURL = process.env.BASE_URL;
 export const writingURL = `${baseURL}/writings`;
 export const categoryURL = `${baseURL}/categories`;
+
+const REDIRECT_URI_DEV = 'http://localhost:3000/oauth'; // 데브 서버용
+const REDIRECT_URI = 'https://donggle.blog/oauth'; // 프로덕션 서버용
+
+export const oauthRedirectURL = (platform: OauthPlatform) =>
+  `${baseURL}/oauth/login/${platform}?redirect_uri=${REDIRECT_URI_DEV}`;
+
+export const oauthLoginURL = (platform: OauthPlatform) => `${baseURL}/oauth/login/${platform}`;

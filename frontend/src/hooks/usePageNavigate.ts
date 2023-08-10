df28441d5579f3adb2609be31497faad0b187a0a
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-10 18:35:53 +0900

    feat: `goHomePage` 함수 구현

--- frontend/src/hooks/usePageNavigate.ts
+++ frontend/src/hooks/usePageNavigate.ts
@@ -3,9 +3,11 @@ import { useNavigate } from 'react-router-dom';
 export const usePageNavigate = () => {
   const navigate = useNavigate();
 
+  const goHomePage = () => navigate('/');
+
   const goWritingPage = (writingId: number) => navigate(`/writing/${writingId}`);
 
   const goWritingTablePage = (categoryId: number) => navigate(`/writings/${categoryId}`);
 
-  return { goWritingPage, goWritingTablePage };
+  return { goHomePage, goWritingPage, goWritingTablePage };
 };

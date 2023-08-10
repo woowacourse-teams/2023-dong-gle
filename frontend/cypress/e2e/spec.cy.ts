0b36a5c0f0782d1c85cc5c6cad0c481944e91383
yogjin <youngjin988@gmail.com>
2023-08-10 15:39:02 +0900

    test: 글 가져오기 모달 창 열기 테스트 추가

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,12 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(false);
+describe('동글 첫 페이지', () => {
+  beforeEach(() => {
+    cy.viewport(1440, 810);
+    cy.visit(`/`);
+  });
+
+  it('Add Post 버튼을 누르면 글 가져오기 모달 창이 열린다.', () => {
+    cy.findByText('Add Post').click();
+
+    cy.findByText('글 가져오기').should('exist');
   });
 });

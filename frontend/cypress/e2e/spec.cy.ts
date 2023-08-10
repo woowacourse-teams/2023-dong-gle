ee7d64c8153fd2fba4701f6006313d6cd73e7021
yogjin <youngjin988@gmail.com>
2023-08-10 21:37:07 +0900

    test: 카테고리 이름 수정 테스트 추가

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -10,6 +10,14 @@ describe('동글 첫 페이지', () => {
     cy.findByText('동글이').should('exist');
   });
 
+  it('카테고리 이름 수정 버튼을 클릭하여 입력 창에 이름을 입력하고 엔터를 쳐서 카테고리 이름을 수정할 수 있다.', () => {
+    cy.findByText('동글이').realHover();
+    cy.findByLabelText('동글이 카테고리 이름 수정').click();
+    cy.findByLabelText('동글이 카테고리 이름 수정 입력 창').focus().type('동글동글이{enter}');
+    cy.findByText('동글이').should('not.exist');
+    cy.findByText('동글동글이').should('exist');
+  });
+
   it('Add Post 버튼을 누르면 글 가져오기 모달 창이 열린다.', () => {
     cy.findByText('Add Post').click();
 

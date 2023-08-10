7f44998f9ed7168a20e0931ea2403b42293bc117
Eun-chan Cho <echo.backend@gmail.com>
2023-08-11 08:58:47 +0900

    [BE] feat: 티스토리 OAuth 연동 (#232)
    * refactor: `KakaoOAuthController` Redirect Code 302 변경
    
    * feat: 티스토리 OAuth 연동
    
    * chore: 티스토리 OAuth 연결에 필요한 환경변수 주입
    
    * refactor: 코드 Style 정리
    
    UriComponentsBuilder를 사용해 한 줄로 길게 늘여진 Uri와 사이사이 변수들을 메서드를 통해 작성할 수 있도록 변경
    
    * refactor: `@Controller` -> `@RestController` 변경
    
    * refactor: `RequestMapping`으로 공통 uri 추출

--- backend/src/main/java/org/donggle/backend/ui/TistoryOAuthController.java
+++ backend/src/main/java/org/donggle/backend/ui/TistoryOAuthController.java
@@ -1,7 +1,7 @@
 package org.donggle.backend.ui;
 
 import lombok.RequiredArgsConstructor;
-import org.donggle.backend.application.service.oauth.kakao.KakaoOAuthService;
+import org.donggle.backend.application.service.oauth.tistory.TistoryOAuthService;
 import org.donggle.backend.application.service.request.OAuthAccessTokenRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,14 +14,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(("/oauth/login/kakao"))
+@RequestMapping("/connections/tistory")
 @RequiredArgsConstructor
-public class KakaoOAuthController {
-    private final KakaoOAuthService kakaoOAuthService;
+public class TistoryOAuthController {
+    private final TistoryOAuthService tistoryOAuthService;
 
     @GetMapping
-    public ResponseEntity<Void> oauthRedirectKakao(@RequestParam final String redirect_uri) {
-        final String redirectUri = kakaoOAuthService.createAuthorizeRedirectUri(redirect_uri);
+    public ResponseEntity<Void> connectionsRedirectTistory(@RequestParam final String redirect_uri) {
+        final String redirectUri = tistoryOAuthService.createAuthorizeRedirectUri(redirect_uri);
         return ResponseEntity
                 .status(HttpStatus.FOUND)
                 .header(HttpHeaders.LOCATION, redirectUri)
@@ -29,8 +29,8 @@ public class KakaoOAuthController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> oauthLoginKakao(@RequestBody final OAuthAccessTokenRequest oAuthAccessTokenRequest) {
-        kakaoOAuthService.login(oAuthAccessTokenRequest);
+    public ResponseEntity<Void> connectionsAddTistory(@RequestBody final OAuthAccessTokenRequest oAuthAccessTokenRequest) {
+        final String accessToken = tistoryOAuthService.getAccessToken(oAuthAccessTokenRequest);
         return ResponseEntity.ok().build();
     }
 }

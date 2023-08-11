ffafa7a2383883bde0cb9f07debe57feff0c19d6
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-11 16:39:33 +0900

    test: trash api handler 추가

---
+++ frontend/src/mocks/handlers/trash.ts
@@ -0,0 +1,19 @@
+import { trashURL } from 'constants/apis/url';
+import { rest } from 'msw';
+
+export const trashHandlers = [
+  // 글 휴지통으로 이동 / 글 영구 삭제
+  rest.post(trashURL, (_, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+
+  // 휴지통에서 글 목록 조회
+  rest.get(trashURL, (_, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+
+  // 휴지통에서 글 복구
+  rest.post(`${trashURL}/restore`, (_, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];

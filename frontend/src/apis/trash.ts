6948d354d9eb9163aba144e9ac63f213d7f1efa1
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-11 16:45:11 +0900

    feat: `trash` api 요청 로직 구현

---
+++ frontend/src/apis/trash.ts
@@ -0,0 +1,27 @@
+import { trashURL } from 'constants/apis/url';
+import { http } from './fetch';
+
+// 글 휴지통으로 이동: POST
+export const moveToTrash = (writingIds: number[]) =>
+  http.post(trashURL, {
+    json: {
+      writingIds,
+      isPermanentDelete: false,
+    },
+  });
+
+// 글 영구 삭제: POST
+export const deletePermanentWritings = (writingIds: number[]) =>
+  http.post(trashURL, {
+    json: {
+      writingIds,
+      isPermanentDelete: true,
+    },
+  });
+
+// 휴지통에 있는 글 목록 조회: GET
+export const getDeletedWritings = () => http.get(trashURL);
+
+// 휴지통에서 글 복구: POST
+export const restoreDeletedWritings = (writingIds: number[]) =>
+  http.post(`${trashURL}/restore`, { json: { writingIds } });

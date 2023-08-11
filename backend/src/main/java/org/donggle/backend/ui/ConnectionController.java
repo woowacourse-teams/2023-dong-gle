7f66fdeb8fdab179768776eb2fee36608bd4d331
ezzanzzan <cjsdmswjd821@naver.com>
2023-08-11 14:23:02 +0900

    style: 컨벤션 적용

--- backend/src/main/java/org/donggle/backend/ui/ConnectionController.java
+++ backend/src/main/java/org/donggle/backend/ui/ConnectionController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/connections")
 @RequiredArgsConstructor
+@RequestMapping("/connections")
 public class ConnectionController {
     private final TistoryOAuthService tistoryOAuthService;
     private final NotionOAuthService notionOAuthService;

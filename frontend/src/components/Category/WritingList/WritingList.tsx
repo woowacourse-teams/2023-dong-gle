9b58feb03a6c4ecbdec75fdfd0227c4b45369b9a
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-11 15:57:04 +0900

    feat: 글 삭제 시 `confirm` 추가

--- frontend/src/components/Category/WritingList/WritingList.tsx
+++ frontend/src/components/Category/WritingList/WritingList.tsx
@@ -6,7 +6,6 @@ import { useWritings } from './useWritings';
 import DeleteButton from 'components/DeleteButton/DeleteButton';
 import { useMutation } from '@tanstack/react-query';
 import { moveToTrash } from 'apis/trash';
-import { MouseEventHandler } from 'react';
 
 type Props = {
   categoryId: number;
@@ -17,14 +16,13 @@ const WritingList = ({ categoryId, isOpen }: Props) => {
   const { goWritingPage } = usePageNavigate();
   const { writings } = useWritings(categoryId, isOpen);
   const writingId = Number(useParams()['writingId']);
-
   const { mutate } = useMutation(moveToTrash, {
     onSuccess: () => alert('글이 휴지통으로 이동됐습니다.'),
     onError: () => alert('글 삭제가 실패했습니다.'),
   });
 
-  const test: MouseEventHandler<HTMLButtonElement> = (e) => {
-    e.stopPropagation();
+  const deleteWriting = () => {
+    if (confirm('글을 삭제하시겠습니까?')) mutate([writingId]);
   };
 
   if (!writings || writings?.length === 0)
@@ -45,7 +43,7 @@ const WritingList = ({ categoryId, isOpen }: Props) => {
             <S.Text>{writing.title}</S.Text>
           </S.Button>
           <S.DeleteButtonWrapper>
-            <DeleteButton onClick={() => mutate([categoryId])} />
+            <DeleteButton onClick={deleteWriting} />
           </S.DeleteButtonWrapper>
         </S.Item>
       ))}

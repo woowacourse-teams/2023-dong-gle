902ec150d3efa01a002b851a3d3061eed0004990
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-13 21:32:32 +0900

    chore: import 순서 수정

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,8 +1,8 @@
 import { categoryHandlers } from './handlers/category';
 import { errorHandlers } from './handlers/error';
-import { loginHandlers } from './handlers/login';
 import { trashHandlers } from './handlers/trash';
 import { writingHandlers } from './handlers/writing';
+import { loginHandlers } from './handlers/login';
 
 export const handlers = [
   ...writingHandlers,

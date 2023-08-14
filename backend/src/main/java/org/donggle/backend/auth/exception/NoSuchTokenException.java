9c2f4b973c17ae9ce5e9985ff52ac7f84ee99a22
ingpyo <juninpyo726@naver.com>
2023-08-14 16:09:52 +0900

    refactor :에러 메시지 오타 수정

--- backend/src/main/java/org/donggle/backend/auth/exception/NoSuchTokenException.java
+++ backend/src/main/java/org/donggle/backend/auth/exception/NoSuchTokenException.java
@@ -3,7 +3,7 @@ package org.donggle.backend.auth.exception;
 import org.donggle.backend.exception.business.BusinessException;
 
 public class NoSuchTokenException extends BusinessException {
-    private static final String MESSAGE = "존재하지 않은 토큰입니다.";
+    private static final String MESSAGE = "존재하지 않는 토큰입니다.";
 
     public NoSuchTokenException() {
         super(MESSAGE);

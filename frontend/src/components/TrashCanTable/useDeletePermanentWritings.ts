4f4f6890e8bda5f2ff829a9eece0e0b4fe03db63
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-15 17:27:14 +0900

    feat: useDeletedPermanentWritings 추가

---
+++ frontend/src/components/TrashCanTable/useDeletePermanentWritings.ts
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deletePermanentWritings as deletePermanentWritingsAPI } from 'apis/trash';
+
+export const useDeletePermanentWritings = () => {
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(deletePermanentWritingsAPI, {
+    onSuccess: () => {
+      alert('글이 삭제되었습니다.');
+      queryClient.invalidateQueries(['deletedWritings']);
+    },
+    onError: () => alert('글 삭제가 실패했습니다.'),
+  });
+
+  const deletePermanentWritings = (writingIds: number[]) => {
+    if (confirm('글을 영구 삭제하시겠습니까?')) mutate(writingIds);
+  };
+
+  return deletePermanentWritings;
+};

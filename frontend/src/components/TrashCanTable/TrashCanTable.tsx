a050fb160ea8e13d01514e5e36a502013785e3ca
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-15 17:30:33 +0900

    feat: `TrashCanTable` 에 훅 추가

--- frontend/src/components/TrashCanTable/TrashCanTable.tsx
+++ frontend/src/components/TrashCanTable/TrashCanTable.tsx
@@ -1,83 +1,84 @@
+import Button from 'components/@common/Button/Button';
 import Input from 'components/@common/Input/Input';
 import { usePageNavigate } from 'hooks/usePageNavigate';
-import { useEffect, useRef, useState } from 'react';
 import { styled } from 'styled-components';
 import { DeletedWriting } from 'types/apis/trash';
+import { useTrashCanTable } from './useTrashCanTable';
 
 type Props = {
   writings: DeletedWriting[];
 };
 
 const TrashCanTable = ({ writings }: Props) => {
-  const [writingIds, setWritingIds] = useState<number[]>([]);
-  const [isClickedAllCheckbox, setIsClickedAllCheckbox] = useState(false);
   const { goWritingPage } = usePageNavigate();
-  const rowRef = useRef<HTMLTableRowElement>(null);
-
-  useEffect(() => {
-    rowRef.current?.focus();
-  }, [writings]);
-
-  const toggleAllCheckbox = () => {
-    if (isClickedAllCheckbox) {
-      // checked 상태 -> 모두 체크 해제
-      setWritingIds([]);
-    } else {
-      // unChecked 상태 -> 모두 체크
-      const allWritingIds = writings.map((writing) => writing.id);
-      setWritingIds(allWritingIds);
-    }
-
-    setIsClickedAllCheckbox((prev) => !prev);
-  };
-
-  const toggleCheckbox = (id: number) => {
-    if (writingIds.includes(id)) {
-      // checked 상태 -> unCheck
-      setWritingIds(writingIds.filter((writingId) => writingId !== id));
-    } else {
-      // unCheck 상태 -> check
-      setWritingIds([...writingIds, id]);
-    }
-  };
+  const {
+    rowRef,
+    deletePermanentWritings,
+    restoreDeletedWritings,
+    toggleAllCheckbox,
+    toggleCheckbox,
+    getIsChecked,
+  } = useTrashCanTable(writings);
 
   return (
-    <S.TrashCanTableContainer summary='카테고리 내부 글 목록을 나타낸다'>
-      <colgroup>
-        <col width='10%' />
-        <col width='90%' />
-      </colgroup>
-      <thead>
-        <tr ref={rowRef} tabIndex={0}>
-          <th>
-            <Input variant='unstyled' type='checkbox' onClick={toggleAllCheckbox} />
-          </th>
-          <th>글 제목</th>
-        </tr>
-      </thead>
-      <tbody>
-        {writings.map(({ id, title, categoryId }) => (
-          <tr key={id} tabIndex={0}>
-            <td>
-              <Input
-                variant='unstyled'
-                type='checkbox'
-                checked={writingIds.includes(id)} // 여기서 체크 상태를 제어합니다.
-                onChange={() => toggleCheckbox(id)}
-              />
-            </td>
-            <td onClick={() => goWritingPage({ categoryId, writingId: id })}>{title}</td>
+    <S.Container>
+      <S.ButtonContainer>
+        <Button variant='secondary' size='small' onClick={deletePermanentWritings}>
+          영구 삭제
+        </Button>
+        <Button variant='secondary' size='small' onClick={restoreDeletedWritings}>
+          글 복구
+        </Button>
+      </S.ButtonContainer>
+      <S.Table summary='휴지통 내부 글 목록을 나타낸다'>
+        <colgroup>
+          <col width='10%' />
+          <col width='90%' />
+        </colgroup>
+        <thead>
+          <tr ref={rowRef} tabIndex={0}>
+            <th>
+              <Input variant='unstyled' type='checkbox' onClick={toggleAllCheckbox} />
+            </th>
+            <th>글 제목</th>
           </tr>
-        ))}
-      </tbody>
-    </S.TrashCanTableContainer>
+        </thead>
+        <tbody>
+          {writings.map(({ id, title, categoryId }) => (
+            <tr key={id} tabIndex={0}>
+              <td>
+                <Input
+                  variant='unstyled'
+                  type='checkbox'
+                  checked={getIsChecked(id)}
+                  onChange={() => toggleCheckbox(id)}
+                />
+              </td>
+              <td onClick={() => goWritingPage({ categoryId, writingId: id })}>{title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </S.Table>
+    </S.Container>
   );
 };
 
 export default TrashCanTable;
 
 const S = {
-  TrashCanTableContainer: styled.table`
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    gap: 2rem;
+  `,
+
+  ButtonContainer: styled.div`
+    display: flex;
+    gap: 0.8rem;
+  `,
+
+  Table: styled.table`
     width: 100%;
     text-align: left;
     font-size: 1.4rem;

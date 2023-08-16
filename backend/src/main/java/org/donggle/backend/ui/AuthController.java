74117f40d5fa9d3a5e2a2d003df909f8339a41b7
echo724 <echo.backend@gmail.com>
2023-08-16 19:51:17 +0900

    refactor: AuthController의 RequestMapping `member` -> `auth` 변경

--- backend/src/main/java/org/donggle/backend/ui/AuthController.java
+++ backend/src/main/java/org/donggle/backend/ui/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final int cookieTime;

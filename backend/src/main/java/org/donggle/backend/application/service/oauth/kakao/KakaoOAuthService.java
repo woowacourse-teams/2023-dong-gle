94765d7eb07652139c16db93ba56a9b73a3df6c0
ingpyo <juninpyo726@naver.com>
2023-08-16 18:14:41 +0900

    refactor: OAuth 로그인 과정 중 에외 처리 로직 추가

--- backend/src/main/java/org/donggle/backend/application/service/oauth/kakao/KakaoOAuthService.java
+++ backend/src/main/java/org/donggle/backend/application/service/oauth/kakao/KakaoOAuthService.java
@@ -1,16 +1,20 @@
 package org.donggle.backend.application.service.oauth.kakao;
 
+import jakarta.persistence.FetchType;
 import org.donggle.backend.application.service.AuthService;
 import org.donggle.backend.application.service.oauth.kakao.dto.KakaoProfileResponse;
 import org.donggle.backend.application.service.oauth.kakao.dto.KakaoTokenResponse;
 import org.donggle.backend.application.service.request.OAuthAccessTokenRequest;
+import org.donggle.backend.application.service.vendor.exception.VendorApiException;
 import org.donggle.backend.ui.response.TokenResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
@@ -22,6 +26,7 @@ public class KakaoOAuthService {
     private static final String PROFILE_URL = "https://kapi.kakao.com/v2/user/me";
     private static final String AUTHORIZE_URL = "https://kauth.kakao.com/oauth/authorize";
     private static final String TOKEN_URL = "https://kauth.kakao.com/oauth/token";
+    private static final String PLATFORM_NAME = "Kakao";
 
     private final String clientId;
     private final String clientSecret;
@@ -65,6 +70,8 @@ public class KakaoOAuthService {
                 .acceptCharset(StandardCharsets.UTF_8)
                 .body(bodyForm)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> VendorApiException.handle4xxException(clientResponse.statusCode().value(),PLATFORM_NAME))
+                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new RuntimeException(clientResponse.toString())))
                 .bodyToMono(KakaoTokenResponse.class)
                 .block();
         return Objects.requireNonNull(kakaoTokenResponse).access_token();

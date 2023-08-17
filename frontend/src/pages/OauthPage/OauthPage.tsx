a6816f14afe9828a3aa4241dd69c85d29cee4421
yogjin <youngjin988@gmail.com>
2023-08-17 14:19:19 +0900

    feat: 로그인 빈 페이지 스타일(Spinner, text) 추가

--- frontend/src/pages/OauthPage/OauthPage.tsx
+++ frontend/src/pages/OauthPage/OauthPage.tsx
@@ -1,9 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
 import { postOauthLogin } from 'apis/login';
 import { OauthPlatforms, getOauthRedirectURL } from 'constants/components/oauth';
+import Spinner from 'components/@common/Spinner/Spinner';
 import { usePageNavigate } from 'hooks/usePageNavigate';
 import { useEffect } from 'react';
 import { useLocation, useSearchParams } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 const OauthPage = () => {
   const { goIntroducePage, goSpacePage } = usePageNavigate();
@@ -43,7 +45,25 @@ const OauthPage = () => {
     });
   }, []);
 
-  return <>Oauth 빈페이지</>;
+  return (
+    <S.SpinnerContainer>
+      <Spinner size={48} thickness={6} />
+      <p>로그인 중입니다...</p>
+    </S.SpinnerContainer>
+  );
 };
 
 export default OauthPage;
+
+const S = {
+  SpinnerContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1.5rem;
+    width: 100vw;
+    height: 100vh;
+    font-size: 1.5rem;
+  `,
+};

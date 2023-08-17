5b37b923dfc5c2c971878d9954358d18d9de6478
yogjin <youngjin988@gmail.com>
2023-08-17 16:42:30 +0900

    feat: 휴지통이 비어있을 때 `TrashCanEmptyIcon` 렌더링

--- frontend/src/pages/TrashCanPage/TrashCanPage.tsx
+++ frontend/src/pages/TrashCanPage/TrashCanPage.tsx
@@ -1,3 +1,4 @@
+import { TrashCanEmptyIcon } from 'assets/icons';
 import TrashCanTable from 'components/TrashCanTable/TrashCanTable';
 import { useDeletedWritings } from 'hooks/useDeletedWritings';
 import { styled } from 'styled-components';
@@ -10,7 +11,14 @@ const TrashCanPage = () => {
   return (
     <S.Article>
       <S.CategoryNameTitle>휴지통</S.CategoryNameTitle>
-      {deletedWritings ? <TrashCanTable writings={deletedWritings} /> : <>휴지통이 비었습니다</>}
+      {deletedWritings?.length ? (
+        <TrashCanTable writings={deletedWritings} />
+      ) : (
+        <S.EmptyMessage>
+          <TrashCanEmptyIcon width={36} height={36} />
+          휴지통이 비어있어요.
+        </S.EmptyMessage>
+      )}
     </S.Article>
   );
 };
@@ -19,6 +27,7 @@ export default TrashCanPage;
 
 const S = {
   Article: styled.article`
+    position: relative;
     width: 100%;
     padding: 8rem;
 
@@ -28,5 +37,17 @@ const S = {
   CategoryNameTitle: styled.h1`
     font-size: 4rem;
     margin-bottom: 5rem;
+  `,
+
+  EmptyMessage: styled.p`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 1.6rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5rem;
   `,
 };

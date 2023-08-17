115e6dfc52204f53905c92a1ef1cc04c5ef0e009
yogjin <youngjin988@gmail.com>
2023-08-17 11:39:33 +0900

    feat: `logout` api 추가

--- frontend/src/apis/login.ts
+++ frontend/src/apis/login.ts
@@ -1,5 +1,6 @@
-import { PostOauthLoginRequest } from 'types/apis/login';
+import { authURL } from 'constants/apis/url';
 import { http } from './fetch';
+import { OauthPlatform, PostOauthLoginRequest, PostOauthLoginResponse } from 'types/apis/login';
 import { OauthPlatforms, getOauthURL, getOauthRedirectURL } from 'constants/components/oauth';
 
 export const getRedirection = (platform: OauthPlatforms) =>
@@ -10,3 +11,5 @@ export const postOauthLogin = ({
   body,
 }: PostOauthLoginRequest): Promise<PostOauthLoginResponse> =>
   http.post(getOauthURL(platform), { json: body });
+
+export const logout = () => http.post(`${authURL}/logout`);

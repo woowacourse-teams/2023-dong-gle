9ab190d25dfdd5527a496f61484625fdafd881cf
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-08-17 22:44:23 +0900

    feat: `useLoginOauth` toast 추가

--- frontend/src/pages/OauthPage/useLoginOauth.ts
+++ frontend/src/pages/OauthPage/useLoginOauth.ts
@@ -3,19 +3,22 @@ import { OauthPlatforms, getOauthRedirectPlatformURL } from 'constants/component
 import { usePageNavigate } from 'hooks/usePageNavigate';
 import { useLocation, useSearchParams } from 'react-router-dom';
 import { loginOauth as loginOauthRequest } from 'apis/login';
+import { useToast } from 'hooks/@common/useToast';
 
 export const useLoginOauth = () => {
   const { goSpacePage, goIntroducePage } = usePageNavigate();
+  const toast = useToast();
   const location = useLocation();
   const [searchParams] = useSearchParams();
   const onError = () => {
-    alert('에러: 로그인을 실패했습니다.');
+    toast.show({ type: 'error', message: '로그인을 실패했습니다' });
     goIntroducePage();
   };
   const { mutate: loginOauthMutation } = useMutation(loginOauthRequest, {
     onSuccess: ({ accessToken }) => {
       localStorage.setItem('accessToken', JSON.stringify(accessToken));
       goSpacePage();
+      toast.show({ type: 'success', message: '로그인을 성공했습니다' });
     },
     onError,
   });

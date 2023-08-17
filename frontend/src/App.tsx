06492e74be6b6d971f4247c6f53f3c5997ab0ff1
yogjin <youngjin988@gmail.com>
2023-08-18 02:34:18 +0900

    feat: development mode일 때만 `ReactQueryDevtools` 렌더링 되도록 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
         </ErrorBoundary>
         <ToastContainer />
       </ToastProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {PRODUCT_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
 };

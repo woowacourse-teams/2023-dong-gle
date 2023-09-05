0d62cde3a232a5f207d41ddc88b78b7dde59dec4
ingpyo <juninpyo726@naver.com>
2023-09-05 16:56:50 +0900

    feat: Oauth request의 따른 예외 처리 및 사용하지 않는 클래스 삭제

--- backend/src/main/java/org/donggle/backend/application/service/oauth/kakao/KakaoOauth2UserInfoClient.java
+++ backend/src/main/java/org/donggle/backend/application/service/oauth/kakao/KakaoOauth2UserInfoClient.java
@@ -2,8 +2,15 @@ package org.donggle.backend.application.service.oauth.kakao;
 
 import org.donggle.backend.application.service.oauth.kakao.dto.KakaoProfileResponse;
 import org.donggle.backend.application.service.oauth.kakao.dto.UserInfo;
+import org.donggle.backend.application.service.vendor.exception.VendorApiException;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+import static org.donggle.backend.application.service.oauth.kakao.SocialType.KAKAO;
 
 @Component
 public class KakaoOauth2UserInfoClient {
@@ -16,12 +23,13 @@ public class KakaoOauth2UserInfoClient {
     }
 
     public UserInfo request(final String accessToken) {
-        final KakaoProfileResponse kakaoProfileResponse = webClient.get()
+        return Objects.requireNonNull(webClient.get()
                 .uri(PROFILE_URL)
                 .header("Authorization", "Bearer " + accessToken)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> VendorApiException.handle4xxException(clientResponse.statusCode().value(), KAKAO.name()))
+                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new RuntimeException(clientResponse.toString())))
                 .bodyToMono(KakaoProfileResponse.class)
-                .block();
-        return kakaoProfileResponse.toUserInfo();
+                .block()).toUserInfo();
     }
 }

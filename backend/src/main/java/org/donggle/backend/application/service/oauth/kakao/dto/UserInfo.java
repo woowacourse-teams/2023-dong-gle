7f2121b681cab721e93f9e3a5ee72975d3779c0c
ingpyo <juninpyo726@naver.com>
2023-09-05 16:51:20 +0900

    feat: Oauth DTO정의

---
+++ backend/src/main/java/org/donggle/backend/application/service/oauth/kakao/dto/UserInfo.java
@@ -0,0 +1,18 @@
+package org.donggle.backend.application.service.oauth.kakao.dto;
+
+import org.donggle.backend.application.service.oauth.kakao.SocialType;
+import org.donggle.backend.domain.member.Member;
+import org.donggle.backend.domain.member.MemberName;
+
+public record UserInfo(
+        Long socialId,
+        SocialType socialType,
+        String nickname
+) {
+    public Member toMember() {
+        return Member.createByKakao(
+                new MemberName(nickname),
+                socialId
+        );
+    }
+}

3c89d00e20001a473851887d82af222e94a8e8e1
ingpyo <juninpyo726@naver.com>
2023-09-05 16:50:42 +0900

    feat: OauthClients 빈등록

--- backend/src/main/java/org/donggle/backend/application/config/VendorConfiguration.java
+++ backend/src/main/java/org/donggle/backend/application/config/VendorConfiguration.java
@@ -1,10 +1,14 @@
 package org.donggle.backend.application.config;
 
+import org.donggle.backend.application.service.oauth.kakao.OauthClient;
+import org.donggle.backend.application.service.oauth.kakao.OauthClients;
 import org.donggle.backend.application.service.vendor.medium.MediumApiService;
 import org.donggle.backend.application.service.vendor.notion.NotionApiService;
 import org.donggle.backend.application.service.vendor.tistory.TistoryApiService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Set;
 
 @Configuration
 public class VendorConfiguration {
@@ -21,5 +25,10 @@ public class VendorConfiguration {
     @Bean
     public NotionApiService notionApiService() {
         return new NotionApiService();
+    }
+
+    @Bean
+    public OauthClients oAuth2Clients(final Set<OauthClient> clients) {
+        return new OauthClients(clients);
     }
 }

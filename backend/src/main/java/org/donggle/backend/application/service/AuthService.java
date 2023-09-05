bed404f80a1830ba22263d4b37ffa638ac49cd3e
ingpyo <juninpyo726@naver.com>
2023-09-05 16:52:32 +0900

    feat: AuthController,AuthService 변경된 로직 추가

--- backend/src/main/java/org/donggle/backend/application/service/AuthService.java
+++ backend/src/main/java/org/donggle/backend/application/service/AuthService.java
@@ -6,13 +6,15 @@ import org.donggle.backend.application.repository.CategoryRepository;
 import org.donggle.backend.application.repository.MemberCredentialsRepository;
 import org.donggle.backend.application.repository.MemberRepository;
 import org.donggle.backend.application.repository.TokenRepository;
-import org.donggle.backend.application.service.oauth.kakao.dto.KakaoProfileResponse;
+import org.donggle.backend.application.service.oauth.kakao.OauthClients;
+import org.donggle.backend.application.service.oauth.kakao.SocialType;
+import org.donggle.backend.application.service.oauth.kakao.dto.UserInfo;
+import org.donggle.backend.application.service.request.OAuthAccessTokenRequest;
 import org.donggle.backend.auth.JwtTokenProvider;
 import org.donggle.backend.auth.RefreshToken;
 import org.donggle.backend.domain.category.Category;
 import org.donggle.backend.domain.member.Member;
 import org.donggle.backend.domain.member.MemberCredentials;
-import org.donggle.backend.domain.member.MemberName;
 import org.donggle.backend.exception.notfound.MemberNotFoundException;
 import org.donggle.backend.ui.response.TokenResponse;
 import org.springframework.stereotype.Service;
@@ -21,24 +23,30 @@ import org.springframework.stereotype.Service;
 @Transactional
 @RequiredArgsConstructor
 public class AuthService {
+    private final OauthClients oauthClients;
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final TokenRepository tokenRepository;
     private final MemberCredentialsRepository memberCredentialsRepository;
     private final CategoryRepository categoryRepository;
 
-    public TokenResponse loginByKakao(final KakaoProfileResponse kakaoProfileResponse) {
-        final Member loginMember = memberRepository.findByKakaoId(kakaoProfileResponse.id())
-                .orElseGet(() -> initializeMember(kakaoProfileResponse));
+    public String createAuthorizeRedirectUri(final String socialType, final String redirect_uri) {
+        return oauthClients.redirectUri(SocialType.from(socialType), redirect_uri);
+    }
 
+    public TokenResponse login(final String socialType, final OAuthAccessTokenRequest request) {
+        final UserInfo userInfo = oauthClients.requestUserInfo(SocialType.from(socialType), request.code(), request.redirect_uri());
+        final Member loginMember = memberRepository.findByKakaoId(userInfo.socialId())
+                .orElseGet(() -> initializeMember(userInfo));
         return createTokens(loginMember);
     }
 
-    private Member initializeMember(final KakaoProfileResponse kakaoProfileResponse) {
-        final Member newMember = memberRepository.save(Member.createByKakao(
-                new MemberName(kakaoProfileResponse.getNickname()),
-                kakaoProfileResponse.id()
-        ));
+    public void logout(final Long memberId) {
+        tokenRepository.deleteByMemberId(memberId);
+    }
+
+    private Member initializeMember(final UserInfo userInfo) {
+        final Member newMember = memberRepository.save(userInfo.toMember());
         final Category basicCategory = Category.basic(newMember);
         final MemberCredentials basic = MemberCredentials.basic(newMember);
 
@@ -61,10 +69,6 @@ public class AuthService {
         synchronizeRefreshToken(loginMember, refreshToken);
 
         return new TokenResponse(accessToken, refreshToken);
-    }
-
-    public void logout(final Long memberId) {
-        tokenRepository.deleteByMemberId(memberId);
     }
 
     private void synchronizeRefreshToken(final Member member, final String refreshToken) {
--- backend/src/main/java/org/donggle/backend/ui/AuthController.java
+++ backend/src/main/java/org/donggle/backend/ui/AuthController.java
@@ -1,49 +1,81 @@
 package org.donggle.backend.ui;
 
 import org.donggle.backend.application.service.AuthService;
+import org.donggle.backend.application.service.request.OAuthAccessTokenRequest;
 import org.donggle.backend.auth.support.AuthenticationPrincipal;
 import org.donggle.backend.ui.response.AccessTokenResponse;
 import org.donggle.backend.ui.response.TokenResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     private static final int ONE_SECOND = 1000;
-    
+
     private final int cookieTime;
     private final AuthService authService;
-    
+
     public AuthController(@Value("${security.jwt.token.refresh-token-expire-length}") final int cookieTime,
                           final AuthService authService) {
         this.cookieTime = cookieTime / ONE_SECOND;
         this.authService = authService;
     }
-    
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@AuthenticationPrincipal final Long memberId) {
-        authService.logout(memberId);
-        return ResponseEntity.ok().build();
+
+    @GetMapping("/oauth/login/{socialType}/redirect")
+    public ResponseEntity<Void> oauthRedirectKakao(
+            @PathVariable final String socialType,
+            @RequestParam final String redirect_uri
+    ) {
+        final String redirectUri = authService.createAuthorizeRedirectUri(socialType, redirect_uri);
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .header(HttpHeaders.LOCATION, redirectUri)
+                .build();
     }
-    
-    @PostMapping("/token/refresh")
-    public ResponseEntity<AccessTokenResponse> reissueAccessToken(@AuthenticationPrincipal final Long memberId) {
-        final TokenResponse response = authService.reissueAccessTokenAndRefreshToken(memberId);
-        
+
+    @PostMapping("/oauth/login/{socialType}")
+    public ResponseEntity<AccessTokenResponse> oauthRedirectKakao(
+            @PathVariable final String socialType,
+            @RequestBody final OAuthAccessTokenRequest oAuthAccessTokenRequest
+    ) {
+        final TokenResponse response = authService.login(socialType, oAuthAccessTokenRequest);
         final ResponseCookie cookie = createRefreshTokenCookie(response.refreshToken());
-        
+
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .header("Set-Cookie", cookie.toString())
                 .body(new AccessTokenResponse(response.accessToken()));
     }
-    
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@AuthenticationPrincipal final Long memberId) {
+        authService.logout(memberId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/token/refresh")
+    public ResponseEntity<AccessTokenResponse> reissueAccessToken(@AuthenticationPrincipal final Long memberId) {
+        final TokenResponse response = authService.reissueAccessTokenAndRefreshToken(memberId);
+
+        final ResponseCookie cookie = createRefreshTokenCookie(response.refreshToken());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("Set-Cookie", cookie.toString())
+                .body(new AccessTokenResponse(response.accessToken()));
+    }
+
     private ResponseCookie createRefreshTokenCookie(final String refreshToken) {
         return ResponseCookie.from("refreshToken", refreshToken)
                 .maxAge(cookieTime)

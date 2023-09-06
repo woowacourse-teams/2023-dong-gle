caad8e2c64c2c515353e7f5eb8829fe73f37279a
ingpyo <juninpyo726@naver.com>
2023-09-06 17:11:04 +0900

    refactor:  추출한 메서드 기준으로 메서드 재정의

--- backend/src/main/java/org/donggle/backend/application/service/vendor/medium/MediumApiClient.java
+++ backend/src/main/java/org/donggle/backend/application/service/vendor/medium/MediumApiClient.java
@@ -1,19 +1,26 @@
 package org.donggle.backend.application.service.vendor.medium;
 
+import org.donggle.backend.application.BlogClient;
+import org.donggle.backend.application.PublishResponse;
 import org.donggle.backend.application.service.vendor.exception.VendorApiException;
 import org.donggle.backend.application.service.vendor.exception.VendorApiInternalServerError;
-import org.donggle.backend.application.service.vendor.medium.dto.request.MediumPublishRequest;
 import org.donggle.backend.application.service.vendor.medium.dto.request.MediumRequestBody;
 import org.donggle.backend.application.service.vendor.medium.dto.response.MediumPublishResponse;
 import org.donggle.backend.application.service.vendor.medium.dto.response.MediumUserResponse;
+import org.donggle.backend.domain.blog.BlogType;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Objects;
 
-public class MediumApiService {
+import static org.donggle.backend.domain.blog.BlogType.MEDIUM;
+
+@Component
+public class MediumApiClient implements BlogClient {
     public static final String PLATFORM_NAME = "Medium";
     private static final String BEARER = "Bearer ";
     private static final String AUTHORIZATION = "Authorization";
@@ -21,18 +28,18 @@ public class MediumApiService {
 
     private final WebClient webClient;
 
-    public MediumApiService() {
+    public MediumApiClient() {
         this.webClient = WebClient.create(MEDIUM_URL);
     }
 
-    public MediumPublishResponse publishContent(final MediumPublishRequest request) {
-        final String mediumToken = request.header().token();
-        final MediumRequestBody body = request.body();
+    @Override
+    public PublishResponse publish(final String accessToken, final String content, final List<String> tags, final String titleValue) {
+        final MediumRequestBody body = makePublishRequest(titleValue, content, tags);
         return webClient.post()
-                .uri("/users/{userId}/posts", getUserId(mediumToken))
+                .uri("/users/{userId}/posts", getUserId(accessToken))
                 .accept(MediaType.APPLICATION_JSON)
                 .acceptCharset(StandardCharsets.UTF_8)
-                .header(AUTHORIZATION, BEARER + mediumToken)
+                .header(AUTHORIZATION, BEARER + accessToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(body)
                 .retrieve()
@@ -40,10 +47,18 @@ public class MediumApiService {
                 .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> clientResponse.bodyToMono(String.class)
                         .map(e -> new VendorApiInternalServerError(PLATFORM_NAME)))
                 .bodyToMono(MediumPublishResponse.class)
-                .block();
+                .block().data().toPublishResponse();
     }
 
-    public String getUserId(final String mediumToken) {
+    private MediumRequestBody makePublishRequest(final String titleValue, final String content, final List<String> tags) {
+        return MediumRequestBody.builder()
+                .title(titleValue)
+                .content(content)
+                .tags(tags)
+                .build();
+    }
+
+    private String getUserId(final String mediumToken) {
         final MediumUserResponse response = webClient.get()
                 .uri("/me")
                 .accept(MediaType.APPLICATION_JSON)
@@ -54,5 +69,10 @@ public class MediumApiService {
                 .bodyToMono(MediumUserResponse.class)
                 .block();
         return Objects.requireNonNull(response).data().id();
+    }
+
+    @Override
+    public BlogType getBlogType() {
+        return MEDIUM;
     }
 }

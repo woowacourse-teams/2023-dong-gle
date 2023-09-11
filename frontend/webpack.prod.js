5d5650afa5ff919e5c1bd3f8283189e1afb795ca
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-11 20:39:27 +0900

    chore: image-minimizer, sharp 플러그인을 이용해 이미지 파일 최적화

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -2,12 +2,39 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'hidden-source-map',
   plugins: [new Dotenv({ path: './.env.production' }), new CompressionPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png)$/i,
+        type: 'asset',
+      },
+    ],
+  },
   optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.sharpMinify,
+          options: {
+            encodeOptions: {
+              webp: {
+                lossless: true,
+              },
+              avif: {
+                lossless: true,
+              },
+              png: {},
+            },
+          },
+        },
+      }),
+    ],
     splitChunks: {
       chunks: 'all',
     },

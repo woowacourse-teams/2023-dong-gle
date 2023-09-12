f002df47de30f2b2c5de9ae541f209dd7d5ecddf
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-12 17:18:04 +0900

    refactor: `loginURL` 상수로 교체

--- frontend/src/constants/components/oauth.ts
+++ frontend/src/constants/components/oauth.ts
@@ -1,4 +1,4 @@
-import { baseURL, domainURL } from 'constants/apis/url';
+import { domainURL, loginURL } from 'constants/apis/url';
 
 export enum OauthPlatforms {
   kakao = 'kakao',
@@ -7,10 +7,5 @@ export enum OauthPlatforms {
 export const getOauthRedirectPlatformURL = (platform: OauthPlatforms) =>
   `${domainURL}/oauth/login/${platform}`;
 
-// 변경 전 url: ${baseURL}/oauth/login/${platform}/redirect?redirect_uri=${getOauthRedirectPlatformURL(platform)}
-// 변경 후 url: ${baseURL}/oauth/login/${platform}?redirect_uri=${getOauthRedirectPlatformURL(platform)}
-// ${baseURL}/oauth/login/${platform} <- '/redirect' 붙고 안 붙고 차이
 export const getOauthPlatformURL = (platform: OauthPlatforms) =>
-  `${baseURL}/auth/login/${platform}/redirect?redirect_uri=${getOauthRedirectPlatformURL(
-    platform,
-  )}`;
+  `${loginURL}/${platform}/redirect?redirect_uri=${getOauthRedirectPlatformURL(platform)}`;

5d903b6d67d446cafbafc84b1fe4735595a6524b
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-12 16:40:37 +0900

    fix: 카카오 리다이렉트 경로 수정

--- frontend/src/constants/components/oauth.ts
+++ frontend/src/constants/components/oauth.ts
@@ -11,6 +11,6 @@ export const getOauthRedirectPlatformURL = (platform: OauthPlatforms) =>
 // 변경 후 url: ${baseURL}/oauth/login/${platform}?redirect_uri=${getOauthRedirectPlatformURL(platform)}
 // ${baseURL}/oauth/login/${platform} <- '/redirect' 붙고 안 붙고 차이
 export const getOauthPlatformURL = (platform: OauthPlatforms) =>
-  `${baseURL}/oauth/login/${platform}/redirect?redirect_uri=${getOauthRedirectPlatformURL(
+  `${baseURL}/auth/login/${platform}/redirect?redirect_uri=${getOauthRedirectPlatformURL(
     platform,
   )}`;

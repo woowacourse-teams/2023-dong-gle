3b20a16bcfd04b8192e80a6c9c58fae0cefdc551
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-14 13:27:55 +0900

    chore: 사용하지 않는 svg 제거

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -3,7 +3,6 @@ export { ReactComponent as CalendarIcon } from './calendar.svg';
 export { ReactComponent as PlusCircleIcon } from './plus-circle.svg';
 export { ReactComponent as SearchIcon } from './search.svg';
 export { ReactComponent as StatsIcon } from './stats.svg';
-export { ReactComponent as CheckSymbol } from './check-symbol.svg';
 export { ReactComponent as TagIcon } from './tag.svg';
 export { ReactComponent as LeftArrowHeadIcon } from './left-arrow-head.svg';
 export { ReactComponent as TistoryLogoIcon } from './tistory-logo.svg';

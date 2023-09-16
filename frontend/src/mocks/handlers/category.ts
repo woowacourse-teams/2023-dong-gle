d0f32bc5381e2a03de041b39308f52a94a2c7ec4
nangkyeonglim <nangkyeonglim@gmail.com>
2023-09-17 02:39:46 +0900

    feat: 글 및 카테고리 순서 수정 API mocking

--- frontend/src/mocks/handlers/category.ts
+++ frontend/src/mocks/handlers/category.ts
@@ -1,7 +1,12 @@
 import { rest } from 'msw';
 import { categoryURL } from 'constants/apis/url';
 import { categories, writingsInCategory, writingsInCategory2 } from 'mocks/categoryContentsMock';
-import { AddCategoriesRequest, PatchCategory } from 'types/apis/category';
+import {
+  AddCategoriesRequest,
+  UpdateCategoryOrderArgs,
+  UpdateCategoryTitleArgs,
+} from 'types/apis/category';
+import { hasDefinedField } from 'utils/typeGuard';
 
 export const categoryHandlers = [
   // 카테고리 목록 조회
@@ -41,36 +46,53 @@ export const categoryHandlers = [
     return res(ctx.json(writingsInCategory2), ctx.delay(300), ctx.status(200));
   }),
 
-  // 카테고리 이름 수정
-  rest.patch(`${categoryURL}/:categoryId`, (req, res, ctx) => {
-    const categoryName = req.body as PatchCategory;
+  // 카테고리 수정(이름, 순서)
+  rest.patch(`${categoryURL}/:categoryId`, async (req, res, ctx) => {
+    const categoryId = Number(req.params.categoryId);
+    const body = await req.json();
 
-    if (!categoryName)
-      return res(
-        ctx.delay(300),
-        ctx.status(404),
-        ctx.json({
-          message: '카테고리 이름 수정 에러',
-        }),
+    // 카테고리 순서 변경
+    if (hasDefinedField<UpdateCategoryOrderArgs['body']>(body, 'nextCategoryId')) {
+      if (!body.nextCategoryId) {
+        return res(
+          ctx.delay(300),
+          ctx.status(404),
+          ctx.json({
+            message: '카테고리 순서 수정 에러',
+          }),
+        );
+      }
+      const newCategories = [...categories.categories];
+
+      const draggingIndex = newCategories.findIndex((category) => category.id === categoryId);
+
+      const draggingItem = newCategories[draggingIndex];
+      newCategories.splice(draggingIndex, 1);
+
+      const dragOverIndex = newCategories.findIndex(
+        (category) => category.id === body.nextCategoryId,
       );
 
-    return res(ctx.delay(300), ctx.status(204));
-  }),
+      if (dragOverIndex === -1) {
+        newCategories.push(draggingItem);
+      } else {
+        newCategories.splice(dragOverIndex, -1, draggingItem);
+      }
+      categories.categories = newCategories;
+    }
 
-  // 카테고리 경로 수정
-  rest.patch(`${categoryURL}/:categoryId`, (req, res, ctx) => {
-    const nextCategoryId = req.body as PatchCategory;
-
-    if (!nextCategoryId)
-      return res(
-        ctx.delay(300),
-        ctx.status(404),
-        ctx.json({
-          message: '카테고리 경로 수정 에러',
-        }),
-      );
-
-    return res(ctx.delay(300), ctx.status(204));
+    // 카테고리 이름 수정
+    if (hasDefinedField<UpdateCategoryTitleArgs['body']>(body, 'categoryName')) {
+      if (!body.categoryName)
+        return res(
+          ctx.delay(300),
+          ctx.status(404),
+          ctx.json({
+            message: '카테고리 이름 수정 에러',
+          }),
+        );
+    }
+    return res(ctx.status(204));
   }),
 
   // 카테고리 삭제

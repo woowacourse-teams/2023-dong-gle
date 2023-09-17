8b42bc8cdda689bb679f38990ca742c8c8668302
Youngjin Park <youngjin988@gmail.com>
2023-09-18 03:18:42 +0900

    feat: `refreshToken` 이용해 `accessToken` 재발급하는 api 추가

--- frontend/src/apis/login.ts
+++ frontend/src/apis/login.ts
@@ -1,8 +1,15 @@
 import { authURL, loginURL } from 'constants/apis/url';
 import { http } from './fetch';
-import { LoginOauthRequest, LoginOauthResponse } from 'types/apis/login';
+import {
+  LoginOauthRequest,
+  LoginOauthResponse,
+  RegenerateAccessTokenResponse,
+} from 'types/apis/login';
 
 export const loginOauth = ({ platform, body }: LoginOauthRequest): Promise<LoginOauthResponse> =>
   http.post(`${loginURL}/${platform}`, { json: body });
 
 export const logout = () => http.post(`${authURL}/logout`);
+
+export const regenerateAccessToken = (): Promise<RegenerateAccessTokenResponse> =>
+  http.post(`${authURL}/token/refresh`);

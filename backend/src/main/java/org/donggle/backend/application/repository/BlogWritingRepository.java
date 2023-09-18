b972ac52909bf343872847669e3f0264471d9ba6
ingpyo <juninpyo726@naver.com>
2023-09-18 14:51:36 +0900

    refactor: 글의 대한 블로그 발행여부 select쿼리 개선

--- backend/src/main/java/org/donggle/backend/application/repository/BlogWritingRepository.java
+++ backend/src/main/java/org/donggle/backend/application/repository/BlogWritingRepository.java
@@ -1,10 +1,16 @@
 package org.donggle.backend.application.repository;
 
 import org.donggle.backend.domain.blog.BlogWriting;
+import org.donggle.backend.domain.writing.Writing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BlogWritingRepository extends JpaRepository<BlogWriting, Long> {
     List<BlogWriting> findByWritingId(final Long writingId);
+
+    @Query("SELECT bw FROM BlogWriting bw LEFT JOIN FETCH bw.blog WHERE bw.writing IN :writings")
+    List<BlogWriting> findWithFetch(@Param("writings") List<Writing> writings);
 }

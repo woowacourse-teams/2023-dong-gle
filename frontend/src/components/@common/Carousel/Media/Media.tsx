fd949b0b79e093b541a5edd5bac505a0a3450026
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-20 01:04:12 +0900

    feat: `Media` 컴포넌트 구현

---
+++ frontend/src/components/@common/Carousel/Media/Media.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export type Media = {
+  type:
+    | 'image/jpeg'
+    | 'image/png'
+    | 'image/webp'
+    | 'image/avif'
+    | 'image/gif'
+    | 'video/mp4'
+    | 'video/webm';
+
+  src: string;
+  alt?: string;
+};
+
+type Props = {
+  media: Media;
+};
+
+const Media = ({ media }: Props) => {
+  const { type, src, alt } = media;
+
+  return (
+    <S.Media>
+      {type.startsWith('video/') ? (
+        <video controls autoPlay playsInline loop width='100%' height='100%'>
+          <source src={src} type={type} />
+          비디오를 지원하지 않는 브라우저입니다.
+        </video>
+      ) : (
+        <picture>
+          <source srcSet={src} type={type} />
+          <img src={src} alt={alt ?? '캐러셀의 컨텐츠'} width='100%' height='100%' />
+        </picture>
+      )}
+    </S.Media>
+  );
+};
+
+export default Media;
+
+const S = {
+  Media: styled.div`
+    width: 100%;
+    height: 100%;
+
+    picture {
+      width: 100%;
+      height: 100%;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
+    video {
+      width: 100%;
+      height: 100%;
+    }
+  `,
+};

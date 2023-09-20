2ae952ce4aeeb8ce99bcfbfedbd2b1c4ff5d997b
yogjin <youngjin988@gmail.com>
2023-09-20 16:48:10 +0900

    refactor: react query select 이용하여 `defaultCategoryId` `activeCategoryIdState` set

--- frontend/src/components/Category/List/useCategories.ts
+++ frontend/src/components/Category/List/useCategories.ts
@@ -4,15 +4,15 @@ import { getCategories } from 'apis/category';
 import { activeCategoryIdState } from 'globalState';
 
 export const useCategories = () => {
-  const { data } = useQuery(['categories'], getCategories);
   const setActiveCategoryIdState = useSetGlobalState(activeCategoryIdState);
-
-  if (data) {
-    const defaultCategoryId = data.categories[0].id;
-
-    localStorage.setItem('defaultCategoryId', String(defaultCategoryId));
-    setActiveCategoryIdState(defaultCategoryId);
-  }
+  const { data } = useQuery(['categories'], getCategories, {
+    select: (data) => {
+      const defaultCategoryId = data.categories[0].id;
+      localStorage.setItem('defaultCategoryId', String(defaultCategoryId));
+      setActiveCategoryIdState(defaultCategoryId);
+      return data;
+    },
+  });
 
   return { categories: data ? data.categories : null };
 };

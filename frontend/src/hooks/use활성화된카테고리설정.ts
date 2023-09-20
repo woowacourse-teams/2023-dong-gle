f7060aedba681fd35d8482caf427748bee18e080
yogjin <youngjin988@gmail.com>
2023-09-20 16:45:44 +0900

    fix: 훅 이름 파일명과 같게 수정

--- frontend/src/hooks/use활성화된카테고리설정.ts
+++ frontend/src/hooks/use활성화된카테고리설정.ts
@@ -2,7 +2,7 @@ import { useSetGlobalState } from '@yogjin/react-global-state';
 import { activeCategoryIdState } from 'globalState';
 import { useEffect } from 'react';
 
-const use활성화된카테고리설정하기 = (categoryId: number) => {
+const use활성화된카테고리설정 = (categoryId: number) => {
   const setActiveCategoryId = useSetGlobalState(activeCategoryIdState);
 
   useEffect(() => {
@@ -13,4 +13,4 @@ const use활성화된카테고리설정하기 = (categoryId: number) => {
   }, [categoryId]);
 };
 
-export default use활성화된카테고리설정하기;
+export default use활성화된카테고리설정;

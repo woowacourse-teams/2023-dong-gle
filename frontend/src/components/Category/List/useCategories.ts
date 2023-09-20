3930b8cb20f3fa0d45cce19e89d2dffe8b11790f
yogjin <youngjin988@gmail.com>
2023-09-20 10:45:28 +0900

    refactor: `defaultCategoryId` 가져올 때 `useEffect` 사용하지 않는 로직으로 수정

--- frontend/src/components/Category/List/useCategories.ts
+++ frontend/src/components/Category/List/useCategories.ts
@@ -1,19 +1,18 @@
 import { useSetGlobalState } from '@yogjin/react-global-state';
 import { useQuery } from '@tanstack/react-query';
 import { getCategories } from 'apis/category';
-import { useEffect } from 'react';
 import { activeCategoryIdState } from 'globalState';
 
 export const useCategories = () => {
   const { data } = useQuery(['categories'], getCategories);
   const setActiveCategoryIdState = useSetGlobalState(activeCategoryIdState);
 
-  useEffect(() => {
-    if (!data) return;
+  if (data) {
+    const defaultCategoryId = data.categories[0].id;
 
-    localStorage.setItem('defaultCategoryId', String(data.categories[0].id));
-    setActiveCategoryIdState(data.categories[0].id);
-  }, [data]);
+    localStorage.setItem('defaultCategoryId', String(defaultCategoryId));
+    setActiveCategoryIdState(defaultCategoryId);
+  }
 
   return { categories: data ? data.categories : null };
 };

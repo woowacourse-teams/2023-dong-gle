2fddb3f8aeafd6d2815f998d028bfcc869439cba
echo724 <echo.backend@gmail.com>
2023-09-20 14:54:24 +0900

    refactor: 사용하지 않는 설정들 제거

--- backend/src/main/java/org/donggle/backend/config/AsyncConfig.java
+++
@@ -1,9 +0,0 @@
-package org.donggle.backend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@Configuration
-@EnableAsync
-public class AsyncConfig {
-}

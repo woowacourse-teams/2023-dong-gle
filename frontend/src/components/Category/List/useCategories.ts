741553baed8ef90cf7f05423907b804e99ad92d1
yogjin <youngjin988@gmail.com>
2023-09-20 17:17:30 +0900

    fix: `defaultCategoryId` `activeCategoryIdState` set 로직 useEffect로 다시 변경
    react query select 방식도 에러가 터지네요

--- frontend/src/components/Category/List/useCategories.ts
+++ frontend/src/components/Category/List/useCategories.ts
@@ -2,17 +2,19 @@ import { useSetGlobalState } from '@yogjin/react-global-state';
 import { useQuery } from '@tanstack/react-query';
 import { getCategories } from 'apis/category';
 import { activeCategoryIdState } from 'globalState';
+import { useEffect } from 'react';
 
 export const useCategories = () => {
   const setActiveCategoryIdState = useSetGlobalState(activeCategoryIdState);
-  const { data } = useQuery(['categories'], getCategories, {
-    select: (data) => {
-      const defaultCategoryId = data.categories[0].id;
-      localStorage.setItem('defaultCategoryId', String(defaultCategoryId));
-      setActiveCategoryIdState(defaultCategoryId);
-      return data;
-    },
-  });
+  const { data } = useQuery(['categories'], getCategories, {});
+
+  useEffect(() => {
+    if (!data) return;
+    const defaultCategoryId = data.categories[0].id;
+
+    localStorage.setItem('defaultCategoryId', String(defaultCategoryId));
+    setActiveCategoryIdState(defaultCategoryId);
+  }, [data]);
 
   return { categories: data ? data.categories : null };
 };

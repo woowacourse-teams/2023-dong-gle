e0214c3cb67d6072a4f59fe7b6eb76c6cfccb150
ingpyo <juninpyo726@naver.com>
2023-09-22 01:58:26 +0900

    feat: 회원 수

---
+++ backend/src/main/java/org/donggle/backend/MemberCount.java
@@ -0,0 +1,23 @@
+package org.donggle.backend;
+
+import org.donggle.backend.application.repository.MemberRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/count")
+public class MemberCount {
+    private final MemberRepository memberRepository;
+
+    public MemberCount(final MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<Integer> asdasdads() {
+        final int i = memberRepository.countMember();
+        return ResponseEntity.ok(i);
+    }
+}
--- backend/src/main/java/org/donggle/backend/application/repository/MemberRepository.java
+++ backend/src/main/java/org/donggle/backend/application/repository/MemberRepository.java
@@ -3,9 +3,18 @@ package org.donggle.backend.application.repository;
 import org.donggle.backend.domain.member.Member;
 import org.donggle.backend.domain.oauth.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findBySocialIdAndSocialType(final Long socialId, final SocialType socialType);
+
+    @Query("""
+            SELECT COUNT(*) AS memberCount
+            FROM Member
+            WHERE Member .createdAt >= '2023-08-19 00:00:00'
+            """)
+    int countMember();
+
 }

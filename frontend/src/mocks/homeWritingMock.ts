3133758cb412beb02d088bc255ea0373291a6263
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-09-21 13:57:20 +0900

    test: 전체글 조회 mock 추가

---
+++ frontend/src/mocks/homeWritingMock.ts
@@ -0,0 +1,507 @@
+import { GetHomeWritingsResponse } from 'types/apis/writings';
+
+export const getHomeWritingMock = (): GetHomeWritingsResponse => {
+  return {
+    content: [
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 200,
+        title: '우테코 레벨 1: 우테코 생활기',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 1,
+          categoryName: '우아한테크코스',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+          {
+            blogName: 'TISTORY',
+            publishedAt: new Date('2023-06-11T06:55:46.922Z'),
+            publishedUrl: 'https://www.tistory.com/',
+          },
+        ],
+      },
+      {
+        id: 34,
+        title: '우테코 레벨 2: 레벨 인터뷰 회고',
+        createdAt: new Date('2023-07-11T06:55:46.922Z'),
+        category: {
+          id: 2,
+          categoryName: '우아한테크코스2',
+        },
+        publishedDetails: [
+          {
+            blogName: 'MEDIUM',
+            publishedAt: new Date('2023-07-11T06:55:46.922Z'),
+            publishedUrl: 'https://medium.com/',
+          },
+        ],
+      },
+    ],
+  };
+};

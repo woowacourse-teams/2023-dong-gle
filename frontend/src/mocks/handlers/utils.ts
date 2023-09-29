3dfd60df9e71b9520b2fbda45a9f775d2ab378fd
yogjin <youngjin988@gmail.com>
2023-09-29 22:51:12 +0900

    refactor: msw 유틸 함수 `withoutJson` 추가 및 `jsonCtx` 함수에 Response 타입 제네릭 추가

--- frontend/src/mocks/handlers/utils.ts
+++ frontend/src/mocks/handlers/utils.ts
@@ -1,7 +1,12 @@
 import { context } from 'msw';
 
-export const jsonCtx = (json: any, statusCode = 200, delay = 500) => [
+export const jsonCtx = <Json>(json: Json, statusCode = 200, delay = 500) => [
   context.delay(delay),
-  context.json(json),
+  context.json<Json>(json),
+  context.status(statusCode),
+];
+
+export const withoutJson = (statusCode = 200, delay = 500) => [
+  context.delay(delay),
   context.status(statusCode),
 ];

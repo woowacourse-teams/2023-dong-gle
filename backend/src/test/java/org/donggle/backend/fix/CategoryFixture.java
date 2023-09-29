2528b41802d7084e92d6b686e68b39d1e80104c8
ingpyo <juninpyo726@naver.com>
2023-09-29 15:16:35 +0900

    test: Category의 순서 테스트 코드 추가

--- backend/src/test/java/org/donggle/backend/fix/CategoryFixture.java
+++ backend/src/test/java/org/donggle/backend/fix/CategoryFixture.java
@@ -9,4 +9,7 @@ public class CategoryFixture {
     public static final Category category = new Category(2L, new CategoryName("카테고리"), null, beaver);
     public static final Category basicCategory = new Category(1L, new CategoryName("기본"), category, beaver);
 
+    public static Category categoryBy(final Long id) {
+        return new Category(id, new CategoryName("카테고리"), null, beaver);
+    }
 }

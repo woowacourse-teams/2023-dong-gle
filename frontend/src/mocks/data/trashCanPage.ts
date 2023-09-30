7e6f27af218231d6855c177e667814240efb06df
yogjin <youngjin988@gmail.com>
2023-09-30 19:55:59 +0900

    feat: 휴지통 관련 모킹(글 삭제, 복구) 핸들러 구체화
    100% 동일한 로직은 아니지만, 겉보기에 그렇게 보이도록 기능 추가

--- frontend/src/mocks/data/trashCanPage.ts
+++ frontend/src/mocks/data/trashCanPage.ts
@@ -1,17 +1,34 @@
+let writingId = 1;
+
 export const trashcanWritingTable = {
   writings: [
     {
-      id: 13,
+      id: writingId++,
       title: '너 버려진거야',
       categoryId: 1,
     },
   ],
 };
 
-export const addDeletedWriting = () => {
+// 글 휴지통으로 이동
+export const moveWritingToTrashTable = () => {
   trashcanWritingTable.writings.push({
-    id: 14,
-    title: '나도 버려졌어',
+    id: writingId++,
+    title: `나도 버려졌어 ${writingId}`,
     categoryId: 1,
   });
+};
+
+// 글 영구 삭제
+export const deleteWritingFromTrashTable = (writingIds: number[]) => {
+  trashcanWritingTable.writings = trashcanWritingTable.writings.filter(
+    ({ id }) => !writingIds.includes(id),
+  );
+};
+
+// 휴지통에서 글 복구
+export const restoreWritingFromTrashTable = (writingIds: number[]) => {
+  trashcanWritingTable.writings = trashcanWritingTable.writings.filter(
+    ({ id }) => !writingIds.includes(id),
+  );
 };
--- frontend/src/mocks/handlers/trash.ts
+++ frontend/src/mocks/handlers/trash.ts
@@ -2,14 +2,28 @@ import { trashURL } from 'constants/apis/url';
 import { ERROR_RESPONSE, isValidAccessToken } from 'mocks/auth';
 import { rest } from 'msw';
 import { jsonCtx, withoutJson } from './utils';
-import { addDeletedWriting, trashcanWritingTable } from 'mocks/data/trashCanPage';
+import {
+  deleteWritingFromTrashTable,
+  moveWritingToTrashTable,
+  restoreWritingFromTrashTable,
+  trashcanWritingTable,
+} from 'mocks/data/trashCanPage';
+import { addRestoredWritings, deleteWritingsIn사이드바카테고리 } from 'mocks/data/category';
+import { DeleteWritingRequest } from 'types/apis/trash';
 
 export const trashHandlers = [
   // 글 휴지통으로 이동 / 글 영구 삭제
-  rest.post(trashURL, (req, res, ctx) => {
+  rest.post(trashURL, async (req, res, ctx) => {
     if (!isValidAccessToken(req)) return res(ctx.status(401), ctx.json(ERROR_RESPONSE));
 
-    addDeletedWriting();
+    const { writingIds, isPermanentDelete } = await req.json<DeleteWritingRequest>();
+
+    if (isPermanentDelete) {
+      deleteWritingFromTrashTable(writingIds);
+    } else {
+      deleteWritingsIn사이드바카테고리(writingIds);
+      moveWritingToTrashTable();
+    }
 
     return res(...withoutJson());
   }),
@@ -22,8 +36,13 @@ export const trashHandlers = [
   }),
 
   // 휴지통에서 글 복구
-  rest.post(`${trashURL}/restore`, (req, res, ctx) => {
+  rest.post(`${trashURL}/restore`, async (req, res, ctx) => {
     if (!isValidAccessToken(req)) return res(ctx.status(401), ctx.json(ERROR_RESPONSE));
+
+    const { writingIds } = await req.json();
+
+    restoreWritingFromTrashTable(writingIds);
+    addRestoredWritings(writingIds);
 
     return res(...withoutJson());
   }),

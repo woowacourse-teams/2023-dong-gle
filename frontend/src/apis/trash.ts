b5ca0285c6947929b376cdb6000229f7261add6f
yogjin <youngjin988@gmail.com>
2023-09-30 19:57:29 +0900

    refactor: 휴지통 api 내부 로직에 `DeleteWritingRequest` 타입이용

--- frontend/src/apis/trash.ts
+++ frontend/src/apis/trash.ts
@@ -1,24 +1,24 @@
 import { trashURL } from 'constants/apis/url';
 import { http } from './fetch';
-import { GetDeletedWritingsResponse } from 'types/apis/trash';
+import { DeleteWritingRequest, GetDeletedWritingsResponse } from 'types/apis/trash';
 
 // 글 휴지통으로 이동: POST
-export const moveToTrash = (writingIds: number[]) =>
-  http.post(trashURL, {
-    json: {
-      writingIds,
-      isPermanentDelete: false,
-    },
-  });
+export const moveToTrash = (writingIds: number[]) => {
+  const json: DeleteWritingRequest = {
+    writingIds,
+    isPermanentDelete: false,
+  };
+  return http.post(trashURL, { json });
+};
 
 // 글 영구 삭제: POST
-export const deletePermanentWritings = (writingIds: number[]) =>
-  http.post(trashURL, {
-    json: {
-      writingIds,
-      isPermanentDelete: true,
-    },
-  });
+export const deletePermanentWritings = (writingIds: number[]) => {
+  const json: DeleteWritingRequest = {
+    writingIds,
+    isPermanentDelete: true,
+  };
+  return http.post(trashURL, { json });
+};
 
 // 휴지통에 있는 글 목록 조회: GET
 export const getDeletedWritings = (): Promise<GetDeletedWritingsResponse> =>

47adc2e28eaf087069d05f18e9dda1cbdafdb556
yogjin <youngjin988@gmail.com>
2023-10-03 20:36:29 +0900

    fix: `get` 메서드가 json을 리턴하도록 수정(ky)

--- frontend/src/apis/blogs.ts
+++ frontend/src/apis/blogs.ts
@@ -4,4 +4,4 @@ import { GetTistoryCategoriesResponse } from 'types/apis/blogs';
 
 // GET: 티스토리 카테고리 요청
 export const getTistoryCategories = (): Promise<GetTistoryCategoriesResponse> =>
-  http.get(`${blogsURL}/tistory/category`);
+  http.get(`${blogsURL}/tistory/category`).json();

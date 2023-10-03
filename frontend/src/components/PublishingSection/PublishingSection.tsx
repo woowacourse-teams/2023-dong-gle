10c18f0d303f90811ed21148fd46c9f70e22fd1b
yogjin <youngjin988@gmail.com>
2023-10-03 20:37:26 +0900

    feat: 발행 플랫폼이 연동되지 않았을 때 발행 가능한 블로그들만 보여주고, 아무 블로그도 연동하지 않았을 때 마이페이지 이동버튼 추가

--- frontend/src/components/PublishingSection/PublishingSection.tsx
+++ frontend/src/components/PublishingSection/PublishingSection.tsx
@@ -3,6 +3,8 @@ import { BLOG_ICON, BLOG_KOREAN, BLOG_LIST } from 'constants/blog';
 import type { Blog } from 'types/domain';
 import Button from 'components/@common/Button/Button';
 import { TabKeys } from 'components/WritingSideBar/WritingSideBar';
+import { useMember } from 'hooks/queries/useMember';
+import { usePageNavigate } from 'hooks/usePageNavigate';
 
 type Props = {
   onBlogButtonClick: (blog: Blog) => void;
@@ -10,6 +12,12 @@ type Props = {
 };
 
 const PublishingSection = ({ onTabClick, onBlogButtonClick }: Props) => {
+  const { tistory, medium } = useMember();
+  const { goMyPage } = usePageNavigate();
+
+  const 블로그가하나라도연결되었는지 = tistory?.isConnected || medium?.isConnected;
+  const 모든블로그가연결되었는지 = tistory?.isConnected && medium?.isConnected;
+
   const openPublishingPropertySection = (blog: Blog) => {
     onBlogButtonClick(blog);
 
@@ -27,22 +35,51 @@ const PublishingSection = ({ onTabClick, onBlogButtonClick }: Props) => {
   return (
     <S.PublishingSection>
       <S.PublishingTitle>발행하기</S.PublishingTitle>
-      <S.BlogPublishButtonList>
-        {Object.values(BLOG_LIST).map((name) => {
-          return (
-            <Button
-              key={name}
-              size='medium'
-              block
-              align='left'
-              icon={BLOG_ICON[name]}
-              onClick={() => openPublishingPropertySection(name)}
-            >
-              {BLOG_KOREAN[name]}
-            </Button>
-          );
-        })}
-      </S.BlogPublishButtonList>
+      {블로그가하나라도연결되었는지 ? (
+        <>
+          <S.BlogPublishButtonList>
+            {Object.values(BLOG_LIST).map((name) => {
+              // 연결 여부 확인을 위한 로직 추가
+              const shouldRenderButton =
+                (name === 'TISTORY' && tistory?.isConnected) ||
+                (name === 'MEDIUM' && medium?.isConnected);
+
+              // 연결이 확인되지 않으면, 버튼을 렌더링하지 않는다.
+              if (!shouldRenderButton) {
+                return null;
+              }
+              // 연결 안된 게 있으면 "다른 블로그 연결하기" 렌더링
+
+              return (
+                <Button
+                  key={name}
+                  size='medium'
+                  block
+                  align='left'
+                  icon={BLOG_ICON[name]}
+                  onClick={() => openPublishingPropertySection(name)}
+                >
+                  {BLOG_KOREAN[name]}
+                </Button>
+              );
+            })}
+          </S.BlogPublishButtonList>
+          {!모든블로그가연결되었는지 && (
+            <S.AddBlogConnectionButton onClick={goMyPage}>
+              블로그 추가로 연결하기
+            </S.AddBlogConnectionButton>
+          )}
+        </>
+      ) : (
+        <S.AddFirstBlogConnectionContainer>
+          <S.AddFirstBlogConnectionText>
+            블로그를 연결해서 글을 발행해보세요!
+          </S.AddFirstBlogConnectionText>
+          <Button variant='secondary' onClick={goMyPage}>
+            연결 하러가기
+          </Button>
+        </S.AddFirstBlogConnectionContainer>
+      )}
     </S.PublishingSection>
   );
 };
@@ -70,5 +107,28 @@ const S = {
 
   ButtonContent: styled.div`
     display: flex;
+  `,
+
+  AddBlogConnectionButton: styled.button`
+    color: ${({ theme }) => theme.color.gray7};
+
+    &:hover {
+      color: ${({ theme }) => theme.color.gray9};
+    }
+
+    &:active {
+      color: ${({ theme }) => theme.color.gray7};
+    }
+  `,
+
+  AddFirstBlogConnectionContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 2rem;
+  `,
+
+  AddFirstBlogConnectionText: styled.p`
+    font-size: 1.5rem;
   `,
 };

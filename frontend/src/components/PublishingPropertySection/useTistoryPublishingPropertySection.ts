97a5c7df8fe7c0cc90e0c27b617fd2f989910ca9
yogjin <youngjin988@gmail.com>
2023-10-03 22:27:53 +0900

    chore: 티스토리 기본 카테고리 코멘트 추가

--- frontend/src/components/PublishingPropertySection/useTistoryPublishingPropertySection.ts
+++ frontend/src/components/PublishingPropertySection/useTistoryPublishingPropertySection.ts
@@ -19,7 +19,7 @@ export const useTistoryPublishingPropertySection = ({ selectCurrentTab }: Args) 
     tags: [],
     publishStatus: 'PUBLIC',
     password: '',
-    categoryId: '0', // TODO: 카테고리 선택 기능 추가
+    categoryId: '0', // 티스토리 기본 카테고리 id === '0'
     publishTime: '',
   });
   const toast = useToast();

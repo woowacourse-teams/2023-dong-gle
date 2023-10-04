5a67ae5db582998988b3d9823f146e6cd03eef5a
HubCreator <chocomilk8604@gmail.com>
2023-10-04 20:23:49 +0900

    test: 테스트 케이스 추가

--- backend/src/test/java/org/donggle/backend/application/service/auth/AuthServiceTest.java
+++ backend/src/test/java/org/donggle/backend/application/service/auth/AuthServiceTest.java
@@ -6,9 +6,14 @@ import org.donggle.backend.domain.auth.JwtTokenProvider;
 import org.donggle.backend.domain.auth.RefreshToken;
 import org.donggle.backend.domain.member.Member;
 import org.donggle.backend.domain.oauth.SocialType;
+import org.donggle.backend.exception.authentication.ExpiredRefreshTokenException;
+import org.donggle.backend.exception.authentication.InvalidRefreshTokenException;
 import org.donggle.backend.infrastructure.oauth.kakao.dto.response.UserInfo;
 import org.donggle.backend.ui.response.TokenResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -21,11 +26,14 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.donggle.backend.domain.oauth.SocialType.KAKAO;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,7 +51,7 @@ class AuthServiceTest {
     @DisplayName("기존에 refreshToken이 있든 없든, accessToken과 refreshToken은 새로 발급된다.")
     @ParameterizedTest
     @MethodSource("provideStringsForIsBlank")
-    void login(Optional<RefreshToken> refreshToken) {
+    void login(final Optional<RefreshToken> refreshToken) {
         //given
         final Member member = mock(Member.class);
         final String newAccessToken = "newAccessToken";
@@ -70,5 +78,67 @@ class AuthServiceTest {
                 Arguments.of(Optional.of(new RefreshToken("oldRefreshToken", null))),
                 Arguments.of(Optional.empty())
         );
+    }
+
+    @Nested
+    class ReissueAccessTokenAndRefreshTokenTest {
+        private Member member;
+        private RefreshToken refreshToken;
+
+        @BeforeEach
+        void setUp() {
+            // given
+            member = mock(Member.class);
+            refreshToken = mock(RefreshToken.class);
+            given(memberRepository.findById(anyLong())).willReturn(Optional.of(member));
+            given(tokenRepository.findByMemberId(anyLong())).willReturn(Optional.of(refreshToken));
+        }
+
+        @ParameterizedTest
+        @DisplayName("refreshToken에 대한 유효성을 검사한다.")
+        @MethodSource("provideValidateRefreshToken")
+        void validateRefreshToken(
+                final boolean isDifferent,
+                final boolean isInvalid,
+                final Class<?> exceptionType
+        ) {
+            // when
+            lenient().when(jwtTokenProvider.getPayload(anyString())).thenReturn(1L);
+            lenient().when(refreshToken.isDifferentFrom(anyString())).thenReturn(isDifferent);
+            lenient().when(jwtTokenProvider.inValidTokenUsage(anyString())).thenReturn(isInvalid);
+
+            // then
+            assertThatThrownBy(() -> authService.reissueAccessTokenAndRefreshToken("oldRefreshToken"))
+                    .isInstanceOf(exceptionType);
+        }
+
+        private static Stream<Arguments> provideValidateRefreshToken() {
+            return Stream.of(
+                    Arguments.of(true, true, Object.class),
+                    Arguments.of(true, false, InvalidRefreshTokenException.class),
+                    Arguments.of(false, true, ExpiredRefreshTokenException.class)
+            );
+        }
+
+        @Test
+        @DisplayName("유효성 검사를 통과하면 accessToken과 refreshToken을 새로 발급한다. (db에 있는 refreshToken은 갱신한다)")
+        void reissue() {
+            //given
+            final String newAccessToken = "newAccessToken";
+            final String newRefreshToken = "newRefreshToken";
+            given(refreshToken.isDifferentFrom(anyString())).willReturn(false);
+            given(jwtTokenProvider.inValidTokenUsage(anyString())).willReturn(false);
+            given(jwtTokenProvider.createAccessToken(anyLong())).willReturn(newAccessToken);
+            given(jwtTokenProvider.createRefreshToken(anyLong())).willReturn(newRefreshToken);
+
+            //when
+            final TokenResponse response = authService.reissueAccessTokenAndRefreshToken("oldRefreshToken");
+
+            //then
+            assertAll(
+                    () -> assertThat(response.accessToken()).isEqualTo(newAccessToken),
+                    () -> assertThat(response.refreshToken()).isEqualTo(newRefreshToken)
+            );
+        }
     }
 }

fb56f883126390e04131906ee1128326a1895fa3
전증훈 <jeonjeunghoon@gmail.com>
2023-10-05 17:20:44 +0900

    [FE] fix: 체크박스 오류 해결 (#541)
    * fix: defaultChecked -> checked, onChange로 수정
    
    * fix: 휴지통 복구, 영구삭제 시 상태 초기화 로직 추가

--- frontend/src/components/TrashCanTable/useRestoreDeletedWritings.ts
+++ frontend/src/components/TrashCanTable/useRestoreDeletedWritings.ts
@@ -3,12 +3,13 @@ import { restoreDeletedWritings as restoreDeletedWritingsAPI } from 'apis/trash'
 import { useToast } from 'hooks/@common/useToast';
 import { HttpError } from 'utils/apis/HttpError';
 
-export const useRestoreDeleteWritings = () => {
+export const useRestoreDeleteWritings = (onSuccessCbFn: () => void) => {
   const queryClient = useQueryClient();
   const toast = useToast();
   const { mutate } = useMutation(restoreDeletedWritingsAPI, {
     onSuccess: () => {
       toast.show({ type: 'success', message: '글이 복구되었습니다.' });
+      onSuccessCbFn();
       queryClient.invalidateQueries(['deletedWritings']);
       queryClient.invalidateQueries(['writingsInCategory']);
     },

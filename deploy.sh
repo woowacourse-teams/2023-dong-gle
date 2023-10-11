d5be185bdf5de2033deab6d7f622ee4bbaf87b7a
Eun-chan Cho <echo.backend@gmail.com>
2023-10-11 17:32:04 +0900

    [ALL] chore: 그린,블루별 서비스 이름 변경 (#578)
    * chore: 그린,블루별 서비스 이름 변경
    
    * chore: 불필요한 변수 제거

--- deploy.sh
+++ deploy.sh
@@ -19,16 +19,12 @@ if [ -z "$EXIST_BLUE" ]; then
     sudo docker compose -p compose-blue -f compose-blue.yml up -d
     BEFORE_COMPOSE_COLOR="green"
     AFTER_COMPOSE_COLOR="blue"
-    export BACKEND_PORT=8080
-    export FRONTEND_PORT=3000
 else
     echo "green up"
     sudo docker compose -p compose-green -f compose-green.yml pull
     sudo docker compose -p compose-green -f compose-green.yml up -d
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
-    export BACKEND_PORT=8081
-    export FRONTEND_PORT=3001
 fi
  
 sleep 10
@@ -53,12 +49,14 @@ else
     exit 1
   fi
 fi
- 
+
+export AFTER_COMPOSE_COLOR 
+
 # 새로운 컨테이너가 제대로 떴는지 확인
 EXIST_AFTER=$(sudo docker compose -p compose-${AFTER_COMPOSE_COLOR} -f compose-${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
 if [ -n "$EXIST_AFTER" ]; then
   # nginx.config를 컨테이너에 맞게 변경해주고 reload 한다
-  envsubst '${FRONTEND_PORT},${BACKEND_PORT}' < conf-${INFRA_PROFILE}/nginx.template > conf-${INFRA_PROFILE}/nginx.conf
+  envsubst '${AFTER_COMPOSE_COLOR}' < conf-${INFRA_PROFILE}/nginx.template > conf-${INFRA_PROFILE}/nginx.conf
   sudo docker compose -f compose-nginx.yml exec nginx nginx -s reload
  
   # 이전 컨테이너 종료
